336e50428ebd8bfd76967fe9cd2dddb542d78200
IAlibay <ialibay@mdanalysis.org>
2022-01-25 12:52:22 +0000

    Add verbosity to CI

--- openfe/tests/test_imports.py
+++ openfe/tests/test_imports.py
@@ -4,7 +4,7 @@ from openfe import setup, orchestration, simulation, analysis
 
 
 @pytest.mark.parametrize('module',
-    ['setup', 'orchestration', 'simulation', 'analysis'])
+        ['setup', 'orchestration', 'simulation', 'analysis'])
 def test_imported(module):
     """Sample test, will always pass so long as import statement worked"""
     assert module in sys.modules
