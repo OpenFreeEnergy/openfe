08ddefb33e7c530c2a61337e8f201315a187cd44
IAlibay <ialibay@mdanalysis.org>
2022-01-25 12:38:23 +0000

    Add some changes requested in PR #1

---
+++ openfe/tests/test_imports.py
@@ -0,0 +1,10 @@
+import sys
+
+from openfe import setup, orchestration, simulation, analysis
+
+
+@pytest.mark.parametrize('module',
+    ['setup', 'orchestration', 'simulation', 'analysis'])
+def test_imported(module):
+    """Sample test, will always pass so long as import statement worked"""
+    assert module in sys.modules
