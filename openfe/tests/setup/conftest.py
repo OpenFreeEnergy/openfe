a606249892f465fdbf0003e9e1df37d2c59de6d1
David W.H. Swenson <david.swenson@omsf.io>
2022-01-26 10:56:47 -0500

    Add base Scorer

---
+++ openfe/tests/setup/conftest.py
@@ -0,0 +1,8 @@
+import pytest
+
+
+@pytest.fixture
+def mock_atommapping():
+    """Mock to functions that take an AtomMapping as input"""
+    # TODO: add internal structure of this once we have an AtomMapping class
+    return "foo"  # current tests using this just need a placeholder
