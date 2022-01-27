fe0ea495108db2ec2f5975f9d4206f4d959540a4
richard <richard@nextmovesoftware.com>
2022-01-27 17:52:23 +0000

    include Molecule in mapping

--- openfe/setup/atommapping.py
+++ openfe/setup/atommapping.py
@@ -1,6 +1,9 @@
 # This code is part of OpenFE and is licensed under the MIT license.
 # For details, see https://github.com/OpenFreeEnergy/openfe
-from typing import Dict, Union
+from rdkit import Chem
+from typing import Dict, Union, TypeVar
+
+RDKitMol = TypeVar('RDKitMol')
 
 
 class AtomMapping:
@@ -8,6 +11,8 @@ class AtomMapping:
 
     Attributes
     ----------
+    molA, molB : rdkit.Mol
+      the two Molecules in the mapping
     AtoB, BtoA : dict
       maps the index of an atom in either molecule **A** or **B** to the other.
       If this atom has no corresponding atom, None is returned.
@@ -15,13 +20,22 @@ class AtomMapping:
 
     The size of molecule A/B is given by the length of the AtoB/BtoA dictionary
     """
+    molA: RDKitMol
+    molB: RDKitMol
     AtoB: Dict[int, Union[int, None]]
     BtoA: Dict[int, Union[int, None]]
 
-    def __init__(self):
+    def __init__(self, molA: RDKitMol, molB: RDKitMol):
+        self.molA = molA
+        self.molB = molB
         self.AtoB = dict()
         self.BtoA = dict()
-    
+
+    def __hash__(self):
+        # hash of SMILES of each molecule, and a hash of both mappings
+        return hash((Chem.MolToSmiles(self.molA), Chem.MolToSmiles(self.molB),
+                     tuple(self.AtoB.items()), tuple(self.BtoA.items())))
+
     @classmethod
     def from_perses(cls, perses_mapping):
         raise NotImplementedError()
--- openfe/tests/setup/test_atommapping.py
+++ openfe/tests/setup/test_atommapping.py
@@ -1,39 +1,67 @@
+from rdkit import Chem
 import pytest
 
 from openfe.setup import AtomMapping
 
 
-@pytest.fixture
+@pytest.fixture(scope='module')
 def simple_mapping():
-    """
-    A0-A1-A2-A3
+    """Disappearing oxygen on end
 
-    to
+    C C O
 
-    B0-B1----B2
+    C C
     """
-    m = AtomMapping()
+    molA = Chem.MolFromSmiles('CCO')
+    molB = Chem.MolFromSmiles('CC')
+
+    m = AtomMapping(molA, molB)
     m.AtoB[0] = 0
     m.AtoB[1] = 1
     m.AtoB[2] = None
-    m.AtoB[3] = 2
 
     m.BtoA[0] = 0
     m.BtoA[1] = 1
-    m.BtoA[2] = 2
+
+    return m
+
+
+@pytest.fixture(scope='module')
+def other_mapping():
+    """Disappearing middle carbon
+
+    C C O
+
+    C   C
+    """
+    molA = Chem.MolFromSmiles('CCO')
+    molB = Chem.MolFromSmiles('CC')
+
+    m = AtomMapping(molA, molB)
+    m.AtoB[0] = 0
+    m.AtoB[1] = None
+    m.AtoB[2] = 1
+
+    m.BtoA[0] = 0
+    m.BtoA[1] = 2
 
     return m
 
 
 def test_atommapping_usage(simple_mapping):
-    assert len(simple_mapping.AtoB) == 4
-    assert len(simple_mapping.BtoA) == 3
+    assert len(simple_mapping.AtoB) == 3
+    assert len(simple_mapping.BtoA) == 2
 
+    assert simple_mapping.AtoB[1] == 1
     assert simple_mapping.AtoB[2] is None
-    assert simple_mapping.AtoB[3] == 2
 
     assert simple_mapping.BtoA[2] == 2
     with pytest.raises(KeyError):
-        simple_mapping.AtoB[4]
+        simple_mapping.AtoB[3]
     with pytest.raises(KeyError):
-        simple_mapping.BtoA[3]
+        simple_mapping.BtoA[2]
+
+
+def test_atommapping_hash(simple_mapping, other_mapping):
+    # these two mappings map the same molecules, but with a different mapping
+    assert simple_mapping is not other_mapping
