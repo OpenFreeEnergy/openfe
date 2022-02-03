d751da65ddd7745f906b5fdb87bf56973902c223
David W.H. Swenson <david.swenson@omsf.io>
2022-02-03 14:50:46 -0500

    Molecule class (#39)
    * Add Molecule class
    
    * fix pep8 blank line issues
    
    * fixes for the easy-to-agree review suggestions
    
    * Update openfe/setup/molecule.py
    
    Co-authored-by: Irfan Alibay <IAlibay@users.noreply.github.com>
    
    Co-authored-by: Irfan Alibay <IAlibay@users.noreply.github.com>

--- openfe/setup/__init__.py
+++ openfe/setup/__init__.py
@@ -1,3 +1,4 @@
+from .molecule import Molecule
 from .atommapping import AtomMapping
 from .alchemical_molecule import AlchemicalMolecule
 from .network import Network
---
+++ openfe/setup/molecule.py
@@ -0,0 +1,32 @@
+# This code is part of OpenFE and is licensed under the MIT license.
+# For details, see https://github.com/OpenFreeEnergy/openfe
+
+from typing import TypeVar
+RDKitMol = TypeVar('RDKitMol')
+
+from openfe.utils.molhashing import hashmol
+
+
+class Molecule:
+    """Molecule wrapper to provide proper hashing and equality.
+
+    Parameters
+    ----------
+    rdkit : rdkit.Mol
+        rdkit representation of the molecule
+    """
+    def __init__(self, rdkit: RDKitMol):
+        self._rdkit = rdkit
+        self._hash = hashmol(self._rdkit)
+
+    # property for immutability; also may allow in-class type conversion
+    @property
+    def rdkit(self) -> RDKitMol:
+        """RDKit representation of this molecule"""
+        return self._rdkit
+
+    def __hash__(self):
+        return hash(self._hash)
+
+    def __eq__(self, other):
+        return hash(self) == hash(other)
