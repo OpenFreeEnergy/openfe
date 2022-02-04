6d14e57143b7566bd6fa2cfcd19f53ea8d58cbff
richard <richard@nextmovesoftware.com>
2022-02-04 15:51:06 +0000

    network planning functions started

--- openfe/setup/network_planning.py
+++ openfe/setup/network_planning.py
@@ -1,52 +1,76 @@
 import math
 
-
-class Network:
-    @classmethod
-    def from_lomap(self, lomap_file):
-        pass
+from . import Network
 
 
-def generate_radial_graph(ligands, central_ligand, mappers, extra_scorers=None):
+def generate_radial_graph(ligands, central_ligand, mappers, scorer=None):
     """Radial Network generator
+
+    Also known as hub and spoke or star-map, this plans a Network where
+    all ligands are connected via a central ligand.
 
     Parameters
     ----------
-    ligands : list of rdkit Molecules
+    ligands : iterable of Molecules
       the ligands to arrange around the central ligand
-    central_ligand : rdkit Molecule
+    central_ligand : Molecule
       the ligand to use as the hub/central ligand
     mappers : iterable of AtomMappers
       mappers to use, at least 1 required
-    extra_scorers : iterable of Scorers, optional
-      extra ways to assign scores
+    scorer : scoring function, optional
+      a callable which returns a float for any AtomMapping.  Used to assign
+      scores to potential mappings, higher scores indicate worse mappings.
+
+    Raises
+    ------
+    ValueError
+      if no mapping between the central ligand and any other ligand can be
+      found
 
     Returns
     -------
     network : Network
-      will have an edge between each ligand and the central ligand, with the mapping
-      being the best possible mapping found using the supplied atom mappers.
+      will have an edge between each ligand and the central ligand, with the
+      mapping being the best possible mapping found using the supplied atom
+      mappers.
+      If no scorer is supplied, the first mapping provided by the iterable
+      of mappers will be used.
     """
-    n = Network()
+    edges = []
 
-    for l in ligands:
-        best_score = inf
+    for ligand in ligands:
+        best_score = math.inf
         best_mapping = None
 
         for mapper in mappers:
-            for mapping in mapper.suggest_mappings(central_ligand, l):
+            for mapping in mapper.suggest_mappings(central_ligand, ligand):
+                if not scorer:
+                    best_mapping = mapping
+                    break
                 score = mapper.score_mapping(mapping)
 
                 if score < best_score:
                     best_mapping = mapping
                     best_score = score
 
-            if best_mapping is None:
-                raise ValueError("No mapping found!")
-            n.add_edge(best_mapping)
+        if best_mapping is None:
+            raise ValueError(f"No mapping found for {ligand}")
+        edges.append(best_mapping)
 
-    raise NotImplementedError
+    return Network(edges)
 
 
-def minimal_spanning_graph(ligands, mappers, extra_scorers=None):
+def minimal_spanning_graph(ligands, mappers, scorer=None):
+    """Plan a Network which connects all ligands with minimal cost
+    Parameters
+    ----------
+    ligands : list of rdkit Molecules
+      the ligands to include in the Network
+    mappers : list of AtomMappers
+      the AtomMappers to use to propose mappings.  At least 1 required,
+      but many can be given, in which case all will be tried to find the
+      lowest score edges
+    scorer : Scoring function
+      any callable which takes an AtomMapping and returns a float
+    """
     raise NotImplementedError
