081a2e5a4f5f66fd34d3fc79340a5d24a28d6962
David W.H. Swenson <david.swenson@omsf.io>
2022-02-04 10:16:59 -0500

    [WIP] Network container class (#40)
    * match later changes
    
    * first draft of Network class
    
    * switch Network.nodes/edges to frozenset
    
    plus a few other minor fixes

--- openfe/setup/network.py
+++ openfe/setup/network.py
@@ -1,52 +1,83 @@
-import math
+from typing import List, TypeVar, Iterable
+from openfe.setup import AtomMapping, Molecule
+
+import networkx as nx
+NetworkType = TypeVar('Network')
 
 
 class Network:
-    @classmethod
-    def from_lomap(self, lomap_file):
-        pass
-
-
-def generate_radial_graph(ligands, central_ligand, mappers, extra_scorers=None):
-    """Radial Network generator
+    """Network container
 
     Parameters
     ----------
-    ligands : list of rdkit Molecules
-      the ligands to arrange around the central ligand
-    central_ligand : rdkit Molecule
-      the ligand to use as the hub/central ligand
-    mappers : iterable of AtomMappers
-      mappers to use, at least 1 required
-    extra_scorers : iterable of Scorers, optional
-      extra ways to assign scores
-
-    Returns
-    -------
-    network : Network
-      will have an edge between each ligand and the central ligand, with the mapping
-      being the best possible mapping found using the supplied atom mappers.
+    edges : Iterable[AtomMapping]
+        edges for this network
+    nodes : Iterable[Molecule]
+        nodes for this network
     """
-    n = Network()
+    def __init__(
+        self, edges: Iterable[AtomMapping], nodes: Iterable[Molecule] = None
+    ):
+        if nodes is None:
+            nodes = []
 
-    for l in ligands:
-        best_score = inf
-        best_mapping = None
+        self._edges = frozenset(edges)
+        edge_nodes = set.union(*[{edge.mol1, edge.mol2} for edge in edges])
+        self._nodes = frozenset(edge_nodes) | frozenset(nodes)
+        self._graph = None
 
-        for mapper in mappers:
-            for mapping in mapper.suggest_mappings(central_ligand, l):
-                score = mapper.score_mapping(mapping)
+    @property
+    def graph(self) -> nx.Graph:
+        """NetworkX graph for this network"""
+        if self._graph is None:
+            graph = nx.MultiDiGraph()
+            for node in self._nodes:
+                graph.add_node(node)
+            for edge in self._edges:
+                graph.add_edge(edge.mol1, edge.mol2, object=edge)
 
-                if score < best_score:
-                    best_mapping = mapping
-                    best_score = score
+            self._graph = nx.freeze(graph)
 
-            if best_mapping is None:
-                raise ValueError("No mapping found!")
-            n.add_edge(best_mapping)
+        return self._graph
 
-    raise NotImplementedError
+    @property
+    def edges(self) -> List[AtomMapping]:
+        """List of edges"""
+        return self._edges
 
+    @property
+    def nodes(self) -> List[Molecule]:
+        """List of nodes"""
+        return self._nodes
 
-def minimal_spanning_graph(ligands, mappers, extra_scorers=None):
-    raise NotImplementedError
+    def enlarge_graph(self, *, edges=None, nodes=None) -> NetworkType:
+        """
+        Create a new network with the given edges and nodes added
+
+        Parameters
+        ----------
+        edges : Iterable[:class:`.AtomMapping`]
+            edges to append to this network
+        nodes : Iterable[:class:`.Molecule`]
+            nodes to append to this network
+
+        Returns
+        -------
+        :class:`.Network :
+            a new network adding the given edges and nodes to this network
+        """
+        if edges is None:
+            edges = set([])
+
+        if nodes is None:
+            nodes = set([])
+
+        return Network(self.edges | set(edges), self.nodes | set(nodes))
+
+    def annotate_node(self, node, annotation) -> NetworkType:
+        """Return a new network with the additional node annotation"""
+        raise NotImplementedError("Waiting on annotations")
+
+    def annotate_edge(self, edge, annotation) -> NetworkType:
+        """Return a new network with the additional edge annotation"""
+        raise NotImplementedError("Waiting on annotations")
