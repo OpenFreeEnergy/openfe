3c9b86dc5194c240095bfe8e7cc75086c829846f
David W.H. Swenson <david.swenson@omsf.io>
2022-02-05 18:20:55 -0500

    tests for Network.enlarge_graph

--- openfe/tests/setup/test_network.py
+++ openfe/tests/setup/test_network.py
@@ -151,5 +151,88 @@ class TestNetwork:
         assert len(network.edges) == container.n_edges
         assert set(network.edges) == set(container.edges)
 
-    def test_enlarge_graph(self, simple_network):
-        pytest.skip("TODO")
+    def test_enlarge_graph_add_nodes(self, simple_network):
+        # New nodes added via ``enlarge_graph`` should exist in the newly
+        # created network
+        new_mol = Molecule(Chem.MolFromSmiles("CCCC"))
+        network = simple_network.network
+        new_network = network.enlarge_graph(nodes=[new_mol])
+        assert new_network is not network
+        assert new_mol not in network.nodes
+        assert new_mol in new_network.nodes
+        assert len(new_network.nodes) == len(network.nodes) + 1
+        assert set(new_network.edges) == set(network.edges)
+
+    def test_enlarge_graph_add_edges(self, mols, simple_network):
+        # New edges added via ``enlarge_graph`` should exist in the newly
+        # created network
+        mol1, _, mol3 = mols
+        extra_edge = AtomMapping(mol1, mol3, {0: 0, 1: 1})
+        network = simple_network.network
+        new_network = network.enlarge_graph(edges=[extra_edge])
+        assert new_network is not network
+        assert extra_edge not in network.edges
+        assert extra_edge in new_network.edges
+        assert len(new_network.edges) == len(network.edges) + 1
+        assert set(new_network.nodes) == set(network.nodes)
+
+    def test_enlarge_graph_add_edges_new_nodes(self, mols, simple_network):
+        # New nodes included implicitly by edges added in ``enlarge_graph``
+        # should exsit in the newly created network
+        new_mol = Molecule(Chem.MolFromSmiles("CCCC"))
+        mol_CC = mols[1]
+        extra_edge = AtomMapping(new_mol, mol_CC, {1: 0, 2: 1})
+        network = simple_network.network
+        new_network = network.enlarge_graph(edges=[extra_edge])
+        assert new_network is not network
+        assert extra_edge not in network.edges
+        assert extra_edge in new_network.edges
+        assert new_mol not in network.nodes
+        assert new_mol in new_network.nodes
+        assert len(new_network.edges) == len(network.edges) + 1
+        assert len(new_network.nodes) == len(network.nodes) + 1
+
+    def test_enlarge_graph_add_duplicate_node(self, simple_network):
+        # Adding a duplicate of an existing node should create a new network
+        # with the same edges and nodes as the previous one.
+        duplicate = Molecule(Chem.MolFromSmiles("CC"))
+        network = simple_network.network
+
+        existing = network.nodes
+        assert duplicate in existing  # matches by ==
+        for mol in existing:
+            # the duplicate is, in fact, not the same object in memory
+            assert mol is not existing
+
+        new_network = network.enlarge_graph(nodes=[duplicate])
+        assert len(new_network.nodes) == len(network.nodes)
+        assert set(new_network.nodes) == set(network.nodes)
+        assert len(new_network.edges) == len(network.edges)
+        assert set(new_network.edges) == set(network.edges)
+
+        for mol in new_network.nodes:
+            # not sure is this needs to be formally required
+            assert mol is not duplicate
+
+    def test_enlarge_graph_add_duplicate_edge(self, mols, simple_network):
+        # Adding a duplicate of an existing edge should create a new network
+        # with the same edges and nodes as the previous one.
+        mol1, _, mol3 = mols
+        duplicate = AtomMapping(mol1, mol3, {0: 0, 2: 1})
+        network = simple_network.network
+
+        existing = network.edges
+        assert duplicate in existing  # matches by ==
+        for edge in existing:
+            # duplicate is not the same object in memory
+            assert duplicate is not existing
+
+        new_network = network.enlarge_graph(edges=[duplicate])
+        assert len(new_network.nodes) == len(network.nodes)
+        assert set(new_network.nodes) == set(network.nodes)
+        assert len(new_network.edges) == len(network.edges)
+        assert set(new_network.edges) == set(network.edges)
+
+        for edge in new_network.edges:
+            # not sure is this needs to be formally required
+            assert edge is not duplicate
