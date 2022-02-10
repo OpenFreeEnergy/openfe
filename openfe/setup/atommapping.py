77a70d0371d54676c5e9a70f08773867e4487e8a
richard <richard@nextmovesoftware.com>
2022-02-10 18:03:03 +0000

    update AtomMapping type annotations
    autoconvert rdkit molecules to Molecules inside AtomMapping
    
    added check to Network node type

--- openfe/setup/atommapping.py
+++ openfe/setup/atommapping.py
@@ -4,6 +4,8 @@ from dataclasses import dataclass
 from rdkit import Chem
 from typing import Dict, Union, TypeVar
 
+from openfe.setup import Molecule
+
 RDKitMol = TypeVar('RDKitMol')
 
 
@@ -13,16 +15,26 @@ class AtomMapping:
 
     Attributes
     ----------
-    mol1, mol2 : rdkit.Mol
+    mol1, mol2 : Molecule
       the two Molecules in the mapping
     mol1_to_mol2 : dict
       maps the index of an atom in either molecule **A** or **B** to the other.
       If this atom has no corresponding atom, None is returned.
 
     """
-    mol1: RDKitMol
-    mol2: RDKitMol
+    mol1: Molecule
+    mol2: Molecule
     mol1_to_mol2: Dict[int, int]
+
+    def __init__(self, mol1: Union[Molecule, RDKitMol],
+                 mol2: Union[Molecule, RDKitMol], mol1_to_mol2):
+        if isinstance(mol1, Chem.Mol):
+            mol1 = Molecule(mol1)
+        self.mol1 = mol1
+        if isinstance(mol2, Chem.Mol):
+            mol2 = Molecule(mol2)
+        self.mol2 = mol2
+        self.mol1_to_mol2 = mol1_to_mol2
 
     def __hash__(self):
         return hash((hash(self.mol1), hash(self.mol2),
