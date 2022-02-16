9e7795a28b37d56430450b8e90632a1abd0c0215
David W.H. Swenson <david.swenson@omsf.io>
2022-02-16 15:20:29 -0500

    Add tests for commands/atommapping

--- openfecli/commands/atommapping.py
+++ openfecli/commands/atommapping.py
@@ -5,7 +5,9 @@ import click
 from openfecli import OFECommandPlugin
 from openfecli.parameters import MOL, MAPPER
 
+
 def allow_two_molecules(ctx, param, value):
+    """click callback to require that --mol is specified exactly twice"""
     if len(value) != 2:
         raise click.BadParameter("Must specify --mol exactly twice.")
     return value
@@ -22,16 +24,18 @@ def atommapping(mol, mapper):
     """
     This provides tools for looking at a specific atommapping.
     """
+    # note that the text of the docstring will be the help when you run
+    # `openfe atommapping --help`.
     mol1_str, mol2_str = mol
     mol1 = MOL.get(mol1_str)
     mol2 = MOL.get(mol2_str)
     mapper_cls = MAPPER.get(mapper)
     mapper_obj = mapper_cls()
-    atom_mapping_print_dict_main(mapper_obj, mol1, mol2)
+    atommapping_print_dict_main(mapper_obj, mol1, mol2)
 
 
 def generate_mapping(mapper, mol1, mol2):
-    mappings = list(mapper.suggest_mappings(mol1.rdkit, mol2.rdkit))
+    mappings = list(mapper.suggest_mappings(mol1, mol2))
     if len(mappings) != 1:
         raise click.UsageError(
             f"Found {len(mappings)} mappings; this command requires a mapper "
@@ -40,7 +44,7 @@ def generate_mapping(mapper, mol1, mol2):
     return mappings[0]
 
 
-def atom_mapping_print_dict_main(mapper, mol1, mol2):
+def atommapping_print_dict_main(mapper, mol1, mol2):
     mapping = generate_mapping(mapper, mol1, mol2)
     print(mapping.mol1_to_mol2)
 
