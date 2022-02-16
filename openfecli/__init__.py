ea6427481315004e89257e7b33b2b9d8b267680c
David W.H. Swenson <david.swenson@omsf.io>
2022-02-16 06:46:15 -0500

    CLI Skeleton (#49)
    * Initial skeleton for the CLI
    
    Currently requires the OPS CLI to be installed as well; next steps:
    
    1. Add parameter core classes to infrastructure
    2. Fully separate infrastructure into its own package
    
    * switch to using plugcli
    
    * tests for CLI
    
    * helps if you add the tests...
    
    * pep8 and deps cleanup
    
    * add test_plugins

---
+++ openfecli/__init__.py
@@ -0,0 +1,5 @@
+# This code is part of OpenFE and is licensed under the MIT license.
+# For details, see https://github.com/OpenFreeEnergy/openfe
+
+from .plugins import OFECommandPlugin
+from . import commands
