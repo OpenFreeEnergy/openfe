81c6e0240dd1d5522bd910e6cd4fcfef1e2f3a58
David W.H. Swenson <david.swenson@omsf.io>
2022-02-21 23:16:53 -0600

    Start to Molecule serialization/deserialization

--- openfe/setup/molecule.py
+++ openfe/setup/molecule.py
@@ -1,10 +1,44 @@
 # This code is part of OpenFE and is licensed under the MIT license.
 # For details, see https://github.com/OpenFreeEnergy/openfe
 
+import io
 from typing import TypeVar
 RDKitMol = TypeVar('RDKitMol')
 
+from rdkit import Chem
+
 from openfe.utils.molhashing import hashmol
+import openfe
+
+
+def _ensure_ofe_name(mol, name):
+    """
+    Ensure that rdkit mol carries the ofe-name and ofe-version tags.
+    """
+    rdkit_name = name  # override this if the property is defined
+    if name == "":
+        try:
+            rdkit_name = mol.GetProp("ofe-name")
+        except KeyError:
+            pass
+
+    if name != "" and rdkit_name != name:
+        raise RuntimeError(f"Internal inconsistency: 'name' has "
+                           f"value '{rdkit_name}' in the internal RDKit "
+                           f"representation; expected '{name}'")
+    elif name == "":
+        name = rdkit_name
+
+    mol.SetProp("ofe-name", name)
+    return name
+
+
+def _ensure_ofe_version(mol):
+    try:
+        version = mol.GetProp("ofe-version")
+    except KeyError:
+        version = openfe.__version__
+        mol.SetProp("ofe-version", version)
 
 
 class Molecule:
@@ -20,6 +54,8 @@ class Molecule:
         will be used in the hash.
     """
     def __init__(self, rdkit: RDKitMol, name: str = ""):
+        name = _ensure_ofe_name(rdkit, name)
+        _ensure_ofe_version(rdkit)
         self._rdkit = rdkit
         self._hash = hashmol(self._rdkit, name=name)
 
@@ -28,6 +64,7 @@ class Molecule:
     def rdkit(self) -> RDKitMol:
         """RDKit representation of this molecule"""
         return self._rdkit
+
 
     @property
     def smiles(self):
@@ -42,3 +79,26 @@ class Molecule:
 
     def __eq__(self, other):
         return hash(self) == hash(other)
+
+    def to_sdf(self) -> str:
+        # https://sourceforge.net/p/rdkit/mailman/message/27518272/
+        mol = self.rdkit
+        sdf = [Chem.MolToMolBlock(mol)]
+        for prop in mol.GetPropNames():
+            # always output as this version of OpenFE
+            if prop == "ofe-version":
+                val = openfe.__version__
+            else:
+                val = mol.GetProp(prop)
+            sdf.append('>  <%s>\n%s\n' % (prop, val))
+        sdf.append('$$$$\n')
+        return "\n".join(sdf)
+
+    @classmethod
+    def from_sdf_string(cls, sdf_str: str):
+        # https://sourceforge.net/p/rdkit/mailman/message/27518272/
+        supp = Chem.SDMolSupplier()
+        supp.SetData(sdf_str)
+        mols = list(supp)
+        assert len(mols) == 1, f"Expected 1 molecule, found {len(mols)}"
+        return cls(rdkit=mols[0])  # name is obtained automatically
