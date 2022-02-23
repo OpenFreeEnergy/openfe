d94fe09f8cb3ecf85a3961c79fef17f6262b598a
richard <richard@nextmovesoftware.com>
2022-02-23 19:05:28 +0000

    tests for atomic_number_rule

--- openfe/tests/setup/test_lomap_scorers.py
+++ openfe/tests/setup/test_lomap_scorers.py
@@ -3,6 +3,7 @@
 
 import math
 import openfe
+from openfe.setup import LomapAtomMapper
 import pytest
 from rdkit import Chem
 
@@ -37,31 +38,63 @@ def toluene_to_heptane(lomap_basic_test_files):
     return openfe.setup.AtomMapping(tolu, hept, mol1_to_mol2=dict(mapping))
 
 
+@pytest.fixture()
+def methylnaphthalene_to_naphthol(lomap_basic_test_files):
+    m1 = lomap_basic_test_files['2-methylnaphthalene']
+    m2 = lomap_basic_test_files['2-naftanol']
+    mapping = [(0, 0), (1, 1), (2, 10), (3, 9), (4, 8), (5, 7), (6, 6), (7, 5),
+               (8, 4), (9, 3), (10, 2)]
+
+    return openfe.setup.AtomMapping(m1, m2, mol1_to_mol2=dict(mapping))
+
+
 class TestScorer:
     def test_mcsr_zero(self, toluene_to_cyclohexane):
-        score = openfe.setup.LomapAtomMapper.mcsr_score(toluene_to_cyclohexane)
+        score = LomapAtomMapper.mcsr_score(toluene_to_cyclohexane)
 
         # all atoms map, so perfect score
         assert score == 0
 
     def test_mcsr_nonzero(self, toluene_to_methylnaphthalene):
-        score = openfe.setup.LomapAtomMapper.mcsr_score(
-            toluene_to_methylnaphthalene)
+        score = LomapAtomMapper.mcsr_score(toluene_to_methylnaphthalene)
 
         assert score == pytest.approx(1 - math.exp(-0.1 * 4))
 
     def test_mcsr_custom_beta(self, toluene_to_methylnaphthalene):
-        score = openfe.setup.LomapAtomMapper.mcsr_score(
-            toluene_to_methylnaphthalene, beta=0.2)
+        score = LomapAtomMapper.mcsr_score(toluene_to_methylnaphthalene,
+                                           beta=0.2)
 
         assert score == pytest.approx(1 - math.exp(-0.2 * 4))
 
     def test_mcnar_score_pass(self, toluene_to_cyclohexane):
-        score = openfe.setup.LomapAtomMapper.mcnar_score(toluene_to_cyclohexane)
+        score = LomapAtomMapper.mcnar_score(toluene_to_cyclohexane)
 
         assert score == 0
 
     def test_mcnar_score_fail(self, toluene_to_heptane):
-        score = openfe.setup.LomapAtomMapper.mcnar_score(toluene_to_heptane)
+        score = LomapAtomMapper.mcnar_score(toluene_to_heptane)
 
         assert score == float('inf')
+
+    def test_atomic_number_score_pass(self, toluene_to_cyclohexane):
+        score = LomapAtomMapper.atomic_number_score(toluene_to_cyclohexane)
+
+        assert score == 0.0
+
+    def test_atomic_number_score_fail(self, methylnaphthalene_to_naphthol):
+        score = LomapAtomMapper.atomic_number_score(
+            methylnaphthalene_to_naphthol)
+
+        # single mismatch @ 0.5
+        assert score == pytest.approx(1 - math.exp(-0.1 * 0.5))
+
+    def test_atomic_number_score_weights(self, methylnaphthalene_to_naphthol):
+        difficulty = {
+            8: {6: 0.75},  # oxygen to carbon @ 12
+        }
+
+        score = LomapAtomMapper.atomic_number_score(
+            methylnaphthalene_to_naphthol, difficulty=difficulty)
+
+        # single mismatch @ (1 - 0.75)
+        assert score == pytest.approx(1 - math.exp(-0.1 * 0.25))
