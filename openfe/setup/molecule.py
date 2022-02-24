3579a011b0e81bf6e7c5652dbb43ec8afb21245f
Richard Gowers <richard@nextmovesoftware.com>
2022-02-24 21:02:38 +0000

    added Molecule conversions (#58)
    Co-authored-by: Mike Henry <11765982+mikemhenry@users.noreply.github.com>
    Co-authored-by: David W.H. Swenson <david.swenson@omsf.io>

--- openfe/setup/molecule.py
+++ openfe/setup/molecule.py
@@ -1,14 +1,22 @@
 # This code is part of OpenFE and is licensed under the MIT license.
 # For details, see https://github.com/OpenFreeEnergy/openfe
+import warnings
+with warnings.catch_warnings():
+    # openff complains about oechem being missing, shhh
+    from openff.toolkit.topology import Molecule as OFFMolecule
 
-from typing import TypeVar
-RDKitMol = TypeVar('RDKitMol')
+from rdkit import Chem
 
 from openfe.utils.molhashing import hashmol
+from openfe.utils.typing import RDKitMol, OEMol
 
 
 class Molecule:
     """Molecule wrapper to provide proper hashing and equality.
+
+    This class is a read-only representation of a molecule, if you want
+    to edit the molecule do this in an appropriate toolkit **before** creating
+    this class.
 
     Parameters
     ----------
@@ -20,14 +28,37 @@ class Molecule:
         will be used in the hash.
     """
     def __init__(self, rdkit: RDKitMol, name: str = ""):
-        self._rdkit = rdkit
+        # internally we store as RDKit, for now...
+        self._rdkit = Chem.Mol(rdkit)
         self._hash = hashmol(self._rdkit, name=name)
 
-    # property for immutability; also may allow in-class type conversion
-    @property
-    def rdkit(self) -> RDKitMol:
-        """RDKit representation of this molecule"""
-        return self._rdkit
+    def to_rdkit(self) -> RDKitMol:
+        """Return an RDKit copied representation of this molecule"""
+        return Chem.Mol(self._rdkit)
+
+    @classmethod
+    def from_rdkit(cls, rdkit: RDKitMol, name: str = ""):
+        """Create a Molecule copying the input from an rdkit Mol"""
+        return cls(rdkit=Chem.Mol(rdkit), name=name)
+
+    def to_openeye(self) -> OEMol:
+        """OEChem representation of this molecule"""
+        return self.to_openff().to_openeye()
+
+    @classmethod
+    def from_openeye(cls, oemol: OEMol, name: str = ""):
+        raise NotImplementedError
+
+    def to_openff(self) -> OFFMolecule:
+        """OpenFF Toolkit representation of this molecule"""
+        m = OFFMolecule(self._rdkit, allow_undefined_stereo=True)
+        m.name = self.name
+
+        return m
+
+    @classmethod
+    def from_openff(cls, openff: OFFMolecule, name: str = ""):
+        raise NotImplementedError
 
     @property
     def smiles(self):
--- openfe/utils/typing.py
+++ openfe/utils/typing.py
@@ -1,6 +1,8 @@
 # This code is part of OpenFE and is licensed under the MIT license.
 # For details, see https://github.com/OpenFreeEnergy/openfe
 
-from . import typing
-from . import molhashing
-from . import errors
+from typing import TypeVar
+
+
+RDKitMol = TypeVar("RDKitMol")
+OEMol = TypeVar('OEMol')
