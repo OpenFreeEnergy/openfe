7e11a8c5814d4d2404a42e239e38c70ca6fb3ff5
richard <richard@nextmovesoftware.com>
2022-02-28 16:55:07 +0000

    added AtomMapping.to_explicit

--- openfe/setup/atommapping.py
+++ openfe/setup/atommapping.py
@@ -1,9 +1,21 @@
 # This code is part of OpenFE and is licensed under the MIT license.
 # For details, see https://github.com/OpenFreeEnergy/openfe
+from __future__ import annotations
+
 from dataclasses import dataclass
+from rdkit import Chem
 from typing import Dict
 
 from openfe.setup import Molecule
+
+
+def _iter_bonded_H(atom):
+    # return iterable of hydrogens bonded to a given rdkit Atom
+    for bond in atom.GetBonds():
+        other = bond.GetOtherAtom(atom)
+        if other.GetAtomicNum() == 1:
+            yield other
+    return
 
 
 @dataclass
@@ -22,6 +34,37 @@ class AtomMapping:
     mol1: Molecule
     mol2: Molecule
     mol1_to_mol2: Dict[int, int]
+
+    def to_explicit(self) -> AtomMapping:
+        """Add explicit hydrogens to this AtomMapping"""
+        rdkit_mol1 = self.mol1.to_rdkit()
+        rdkit_mol2 = self.mol2.to_rdkit()
+
+        # rdkit uses 0 as a null value for atommap, so we need to use 1-based
+        # indexing here
+        for i, atom in enumerate(rdkit_mol1.GetAtoms()):
+            atom.SetAtomMapNum(i+1)
+        for i, atom in enumerate(rdkit_mol2.GetAtoms()):
+            atom.SetAtomMapNum(i+1)
+
+        explicit_mol1 = Chem.AddHs(rdkit_mol1)
+        explicit_mol2 = Chem.AddHs(rdkit_mol2)
+
+        newmapping = self.mol1_to_mol2.copy()
+        # loop over atoms in the implicit-H mapping,
+        # then try and match up hydrogens as best as possible
+        for i, j in self.mol1_to_mol2.items():
+            atom_i = explicit_mol1.GetAtomWithIdx(i)
+            atom_j = explicit_mol2.GetAtomWithIdx(j)
+
+            # TODO: Could probably do better by geometrically matching
+            for i_H, j_H in zip(_iter_bonded_H(atom_i),
+                                _iter_bonded_H(atom_j)):
+                newmapping[i_H.GetIdx()] = j_H.GetIdx()
+
+        return AtomMapping(Molecule(explicit_mol1, name=self.mol1.name),
+                           Molecule(explicit_mol2, name=self.mol2.name),
+                           mol1_to_mol2=newmapping)
 
     def __hash__(self):
         return hash((hash(self.mol1), hash(self.mol2),
