d6091e669a566d70617fc160224db03a4beb5902
richard <richard@nextmovesoftware.com>
2022-02-28 16:57:55 +0000

    added to_explicit test

--- openfe/tests/setup/test_atommapping.py
+++ openfe/tests/setup/test_atommapping.py
@@ -12,3 +12,11 @@ def test_atommapping_usage(simple_mapping):
 def test_atommapping_hash(simple_mapping, other_mapping):
     # these two mappings map the same molecules, but with a different mapping
     assert simple_mapping is not other_mapping
+
+
+def test_to_explicit(simple_mapping):
+    explicit_simple_mapping = simple_mapping.to_explicit()
+
+    # CH3-CH3 -> CH3-CH2-OH
+    # both carbons map, and 5 hydrogens, so 7?
+    assert len(explicit_simple_mapping.mol1_to_mol2) == 7
