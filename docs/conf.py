f4fadcc39ae5dee2e2d032440b6f5c84984dd39e
richard <richard@nextmovesoftware.com>
2022-03-03 16:25:30 +0000

    docs: mock out lomap

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-autodoc_mock_imports = ['networkx', 'rdkit', 'openff', 'openff.toolkit', 'openeye']
+autodoc_mock_imports = ['networkx', 'lomap', 'openff', 'openff.toolkit', 'openeye', 'rdkit']
 
 
 # -- Options for HTML output -------------------------------------------------
