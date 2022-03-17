719903c19602bed37ff44c35482a4ecc7a8d3b35
David W.H. Swenson <david.swenson@omsf.io>
2022-03-17 14:59:14 -0500

    Rename typing.py -> custom_typing.py

--- openfe/utils/__init__.py
+++ openfe/utils/__init__.py
@@ -1,6 +1,13 @@
 # This code is part of OpenFE and is licensed under the MIT license.
 # For details, see https://github.com/OpenFreeEnergy/openfe
 
-from . import typing
+from . import custom_typing as typing
 from . import molhashing
 from . import errors
+
+# without this, `from openfe.utils.typing` raises a ModuleNotFound error --
+# the import above only gives us the ability to do `from openfe.utils import
+# typing`.
+import sys
+sys.modules['openfe.utils.typing'] = typing
+del sys
