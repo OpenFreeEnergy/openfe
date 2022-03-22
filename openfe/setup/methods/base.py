feb616fc2e1e08827973d02cdfda046ba01b3100
richard <richard@nextmovesoftware.com>
2022-03-22 15:17:10 +0000

    use abc

--- openfe/setup/methods/base.py
+++ openfe/setup/methods/base.py
@@ -1,8 +1,10 @@
 # This code is part of OpenFE and is licensed under the MIT license.
 # For details, see https://github.com/OpenFreeEnergy/openfe
 
+import abc
 
-class FEMethod:
+
+class FEMethod(abc.ABC):
     """Base class for defining a free energy method
 
     Child classes must implement:
@@ -11,27 +13,30 @@ class FEMethod:
     - run()
     -
     """
-    _SETTINGS_CLASS = None
-
     @classmethod
     def get_default_settings(cls):
         """Get the default settings for this FE Method
 
         These can be modified and passed back in to the class init
         """
-        if cls._SETTINGS_CLASS is not None:
-            return cls._SETTINGS_CLASS()
-        else:
-            raise NotImplementedError()
+        return cls._SETTINGS_CLASS()
 
+    @abc.abstractmethod
     def to_xml(self) -> str:
         """Serialise this method to xml"""
         raise NotImplementedError()
 
     @classmethod
+    @abc.abstractmethod
     def from_xml(cls, xml: str):
         raise NotImplementedError()
 
+    @abc.abstractmethod
     def is_complete(self) -> bool:
         """Check if the results of this workload already exist"""
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def run(self) -> bool:
+        """Perform this method, returning success"""
         raise NotImplementedError()
