3c64c343c4d09bcf7da696fd40decd7b21e8fe45
IAlibay <ialibay@mdanalysis.org>
2022-04-01 16:33:04 +0100

    first pass

--- openfe/setup/methods/rbfe_methods.py
+++ openfe/setup/methods/rbfe_methods.py
@@ -19,43 +19,184 @@ from openff.units import unit
 unit.define('timestep = [timestep] = _ = timesteps')
 
 
-class ForcefieldSettings(BaseModel):
-    """Settings describing the forcefield to use for each component
+class SystemSettings(BaseModel):
+    """Settings describing the simulation system settings.
 
     Attributes
     ----------
-    forcefield
-      a mapping of each components name to the xml forcefield to apply
-    nonbonded_electrostatics
-      which nonbonded electrostatics method to use, currently only 'PME'
-      allowed
-    nonbonded_cutoff
-      range of nonbonded forces
-    constraints
-      which bonds to apply constraints to
-    rigid_water
-      whether apply rigid constraints to water molecules, default True
+    nonbonded_method : str
+        Which nonbonded electrostatic method to use, currently only PME
+        is supported.
+    nonbonded_cutoff : float * unit.nanometer
+        Cutoff value for short range interactions.
+        Default 1.0 * unit.nanometer.
+    constraints : str
+        Which bonds and angles should be constrained. Default None.
+    rigid_water : bool
+        Whether to apply rigid constraints to water molecules. Default True.
+    hydrogen_mass : float
+        How much mass to repartition to hydrogen. Default None, no
+        repartitioning will occur.
     """
     class Config:
         arbitrary_types_allowed = True
 
+    nonbonded_method = 'PME'
+    nonbonded_cutoff = 1.0 * unit.nanometer
+    constraints = Union[str, None] # Usually use HBonds
+    rigid_water = True
+    remove_com = True # Probably want False here
+    hydrogen_mass = Union[float, None]
+
+
+class SolventBoxSettings(BaseModel):
+    """Settings for the creation of the simulation box, inc. solvation
+    and addition of ions
+
+    Attributes
+    ----------
+    model : str
+      The water model to use. Default 'tip3p'.
+    padding : float
+      Padding distance from the solute to the edge of the box. Default None.
+    ionic_strength : float
+      Total concentration of ions (both positive and negative) to add,
+      excluding the ions required to neutralize the system.
+      Default 0 * unit.molar.
+    num_added : int
+      Total number of molecules (waters and ions) to add. Default None.
+    positive_ion : str
+      Type of positive ion to add, only monovalent ions are supported.
+      Default 'Na+'.
+    negative_ion : str
+      Type of negative ion to add, only monovalent ions are supported.
+      Default 'Cl-'.
+    neutralize : float
+      Whether to add ions to neutralize the system. Default True.
+    box_vectors : tuple of Vec3
+      User defined box vectors to fill with water. Default None.
+    box_size : Vec3
+      User defined box size to fill with water. Default None.
+    """
+    model = 'tip3p'
+    padding = Union[float, None]
+    ionic_strength = 0 * unit.molar
+    num_added = Union[int, None]
+    positive_ion = 'Na+'
+    negative_ion = 'Cl-'
+    neutralize = True
+    box_vectors = Union[tuple(Vec3), None]
+    box_size = Union[Vec3, None]
+
+
+class TopologySettings(BaseModel):
+    """Settings for creating Topologies for each component
+
+    Attributes
+    ----------
+    forcefield: dictionary of list of strings
+      A mapping of each components name to the xml forcefield to apply
+    """
     # mapping of component name to forcefield path(s)
     forcefield: Dict[str, Union[List[str, ...], str]]
-    nonbonded_electostatics = 'PME'
-    nonbonded_cutoff = 0.9 * unit.nanometer
-    constraints = 'HBonds'
-    rigid_water = True
 
 
-class StatePoint(BaseModel):
-    """Description of the temperature and pressure to model"""
+class AlchemicalSettings(BaseModel):
+    """Settings for the alchemical protocol
+
+    This describes the lambda schedule and the creation of the
+    hybrid system.
+
+    Attributes
+    ----------
+    lambda_functions : str, default 'default'
+      Key of which switching functions to use for alchemical mutation.
+      Default 'default'.
+    lambda_windows : int
+      Number of lambda windows to calculate. Default 11.
+    unsample_endstate : bool
+      Whether to have extra unsampled endstate windows for long range
+      correction. Default False.
+    use_dispersion_correction: bool
+      Whether to use dispersion correction in the hybrid topology state. Default False.
+    softcore_LJ_v2 : bool
+      Whether to use the LJ softcore function as defined by
+      Gapsys et al. JCTC 2012 Default True.
+    softcore_alpha : float
+      Softcore alpha parameter. Default 0.85
+    softcore_electrostatics : bool
+      Whether to use softcore electrostatics. Default True.
+    sofcore_electorstatics_alpha : float
+      Softcore alpha parameter for electrostatics. Default 0.3
+    softcore_sigma_Q : float
+      Softcore sigma parameter for softcore electrostatics. Default 1.0.
+    interpolate_old_and_new_14s : bool
+      Whether to turn off interactions for new exceptions (not just 1,4s)
+      at lambda 0 and old exceptions at lambda 1. If False they are present
+      in the nonbonded force. Default False.
+    flatten_torsions : bool
+      Whether to scale torsion terms involving unique atoms, such that at the
+      endstate the torsion term is turned off/on depending on the state the
+      unique atoms belong to.
+    """
+    # Lambda settings
+    lambda_functions = 'default'
+    lambda_windows = 11
+    unsampled_endstates = False
+
+    # alchemical settings
+    use_dispersion_correction = False
+    softcore_LJ_v2 = True
+    softcore_electrostatics = True
+    softcore_alpha = 0.85
+    softcore_electrostatics_alpha = 0.3
+    softcore_sigma_Q = 1.0
+    interpolate_old_and_new_14s = False
+    flatten_torsions = False
+
+
+class SimulationSettings(BaseModel):
+    pass
+
+class OpenMMEngineSettings(BaseModel):
+    """OpenMM MD engine settings
+
+    Attributes
+    ----------
+    compute_platform : str
+      Which compute platform to perform the simulation on. If 'fastest', the
+      fastest compute platform available will be chosen. Default 'fastest'.
+     
+    TODO
+    ----
+    * In the future make precision and deterministic forces user defined too.
+    """
+    compute_platform = 'fastest'
+
+
+class LangevinIntegratorSettings(BaseModel):
+    temperature = 298.15 * unit.kelvin
+
+    @validator('temperature')
+    def must_be_positive(cls, v):
+        if v <= 0:
+            raise ValueError("Pressure and temperature must be positive")
+        return v
+
+    @validator('temperature')
+    def is_temperature(cls, v):
+        if not v.is_compatible_with(unit.kelvin):
+            raise ValueError("Must be temperature value, e.g. use unit.kelvin")
+
+
+class BarostatSettings(BaseModel):
     class Config:
         arbitrary_types_allowed = True
 
-    temperature = 298.15 * unit.kelvin
     pressure = 1 * unit.bar
+    frequency = 50 * unit.timestep
 
-    @validator('pressure', 'temperature')
+    @validator('pressure')
     def must_be_positive(cls, v):
         if v <= 0:
             raise ValueError("Pressure and temperature must be positive")
@@ -66,47 +207,6 @@ class StatePoint(BaseModel):
         if not v.is_compatible_with(unit.bar):
             raise ValueError("Must be pressure value, e.g. use unit.bar")
         return v
-
-    @validator('temperature')
-    def is_temperature(cls, v):
-        if not v.is_compatible_with(unit.kelvin):
-            raise ValueError("Must be temperature value, e.g. use unit.kelvin")
-
-
-class LambdaProtocolSettings(BaseModel):
-    """Settings for the lambda protocol
-
-    This describes a fixed number of windows
-
-    Attributes
-    ----------
-    functions : str, default 'default'
-      key of which switching functions to use for alchemical mutation
-    windows : int, default 11
-      number of lambda windows to calculate
-    sample_endstate : bool, default False
-      whether to sample the endstates ???
-    """
-    functions = 'default'
-    windows = 11
-    sample_endstates = False
-
-
-class MonteCarloBarostatSettings(BaseModel):
-    """Settings for the OpenMM Monte-Carlo Barostat
-
-    The temperature and pressure value is taken from the StatePoint variable
-
-    Attributes
-    ----------
-    frequency : unit.timestep
-      the number of timesteps between attempts of changing the pressure
-    """
-    class Config:
-        extra = 'forbid'
-        arbitrary_types_allowed = True
-
-    frequency = 50 * unit.timestep
 
 
 class MCMCLangevinSplittingDynamicsMoveSettings(BaseModel):
@@ -137,18 +237,6 @@ class MCMCLangevinSplittingDynamicsMoveSettings(BaseModel):
     constraint_tolerance = 1e-06
 
 
-class HybridTopologyFactorySettings(BaseModel):
-    use_dispersion_correction = False
-    softcore_alpha = 0.5
-    softcore_LJ_v2 = True
-    softcore_LJ_v2_alpha = 0.85
-    softcore_electrostatics = True
-    softcore_electrostatics_alpha = 0.3
-    softcore_sigma_Q = 1.0
-    interpolate_old_and_new_14s = False
-    flatten_torsions = False
-
-
 class SimulationLengthSettings(BaseModel):
     class Config:
         arbitrary_types_allowed = True
@@ -169,7 +257,7 @@ class SimulationLengthSettings(BaseModel):
         return v
 
 
-class LigandLigandTransformSettings(BaseModel):
+class RelativeLigandTransformSettings(BaseModel):
     class Config:
         arbitrary_types_allowed = True
 
@@ -188,12 +276,12 @@ class LigandLigandTransformSettings(BaseModel):
     simulation_length: SimulationLengthSettings
 
 
-class LigandLigandTransformResults:
+class RelativeLigandTransformResults:
     """Dict-like container for the output of a LigandLigandTransform"""
     pass
 
 
-class LigandLigandTransform(FEMethod):
+class RelativeLigandTransform(FEMethod):
     """Calculates the free energy of an alchemical ligand swap in solvent
 
     """
