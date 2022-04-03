fbda820d9d71a9186e61be7159edcd72107b3603
richard <richard@nextmovesoftware.com>
2022-04-03 10:15:38 +0100

    imports from gufe

--- openfe/setup/__init__.py
+++ openfe/setup/__init__.py
@@ -1,7 +1,13 @@
 # This code is part of OpenFE and is licensed under the MIT license.
 # For details, see https://github.com/OpenFreeEnergy/openfe
 
-from gufe import SmallMoleculeComponent
+from gufe import (
+    ChemicalState,
+    Component,
+    ProteinComponent,
+    SmallMoleculeComponent,
+    SolventComponent,
+)
 
 from .ligandatommapping import LigandAtomMapping
 from .ligandatommapper import LigandAtomMapper
@@ -12,4 +18,3 @@ from .lomap_mapper import LomapAtomMapper
 from . import ligand_network_planning
 
 from . import methods
-
