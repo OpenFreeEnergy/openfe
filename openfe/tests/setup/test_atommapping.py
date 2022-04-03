166f12d7fd1ca6e0ed48e67da48f124da66b6375
IAlibay <ialibay@mdanalysis.org>
2022-04-03 22:29:22 +0100

    Add basic serialization tests (mirrors SmallMolecule tests in guefe)

--- openfe/tests/setup/test_atommapping.py
+++ openfe/tests/setup/test_atommapping.py
@@ -1,6 +1,9 @@
 import pytest
 import pathlib
+import json
 from rdkit import Chem
+
+from openfe.setup import LigandAtomMapping
 
 
 def test_atommapping_usage(simple_mapping):
@@ -29,3 +32,34 @@ def test_draw_mapping_svg(tmpdir, other_mapping):
         other_mapping.draw_to_file('test.svg', d2d=d2d)
         filed = pathlib.Path('test.svg')
         assert filed.exists()
+
+
+class TestLigandAtomMappingSerialization:
+    def test_to_dict(self, simple_mapping):
+        d = simple_mapping.to_dict()
+
+        assert isinstance(d, dict)
+
+    def test_deserialize_roundtrip(self, simple_mapping, other_mapping):
+
+        roundtrip = LigandAtomMapping.from_dict(simple_mapping.to_dict())
+
+        assert roundtrip == simple_mapping
+
+        # TODO: Check that molA and molB coordinates haven't changed
+
+        assert roundtrip != other_mapping
+
+    def test_file_roundtrip(self, simple_mapping, tmpdir):
+
+        with tmpdir.as_cwd():
+            with open('tmpfile.json', 'w') as f:
+                f.write(smple_mapping.to_json())
+
+            with open('tmpfile.json', 'r') as f:
+                d = json.load(f)
+
+            assert isinstance(d, dict)
+            roundtrip = LigandAtomMapping.from_dict(d)
+
+            assert roundtrip == simple_mapping
