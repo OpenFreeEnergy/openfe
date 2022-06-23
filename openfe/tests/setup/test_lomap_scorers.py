b652e051ac932a30889e88a368e0020b6e7b83b7
Richard Gowers <richard@nextmovesoftware.com>
2022-06-23 05:41:32 -0700

    change Lomap atom mapper to default to threed=True (#143)
    * change Lomap atom mapper to default to threed=True
    
    * fix regression tests to use threed=False for lomap comparison

--- openfe/tests/setup/test_lomap_scorers.py
+++ openfe/tests/setup/test_lomap_scorers.py
@@ -186,7 +186,7 @@ def test_heterocycle_score(base, other, name, hit):
     m1 = openfe.setup.SmallMoleculeComponent.from_rdkit(r1)
     m2 = openfe.setup.SmallMoleculeComponent.from_rdkit(r2)
 
-    mapper = openfe.setup.LomapAtomMapper()
+    mapper = openfe.setup.LomapAtomMapper(threed=False)
     mapping = next(mapper.suggest_mappings(m1, m2))
     score = lomap_scorers.heterocycles_score(mapping)
 
@@ -220,7 +220,7 @@ def test_lomap_individual_scores(params,
                                       molB.to_rdkit()), scorename)()
 
     # longer way
-    mapper = openfe.setup.LomapAtomMapper()
+    mapper = openfe.setup.LomapAtomMapper(threed=False)
     mapping = next(mapper.suggest_mappings(molA, molB))
     openfe_version = getattr(lomap_scorers, SCORE_NAMES[scorename])(mapping)
 
@@ -245,7 +245,7 @@ def test_lomap_regression(lomap_basic_test_files_dir,  # in a dir for lomap
         nm = dbmols[i].getName()
         smallmols.append(lomap_basic_test_files[nm[:-5]])  # - ".mol2"
 
-    mapper = openfe.setup.LomapAtomMapper()
+    mapper = openfe.setup.LomapAtomMapper(threed=False)
     scorer = lomap_scorers.default_lomap_score
     scores = np.zeros_like(matrix)
     for i, j in itertools.combinations(range(matrix.shape[0]), 2):
