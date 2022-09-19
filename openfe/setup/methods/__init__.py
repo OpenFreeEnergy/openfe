35b0b3c31c07e864822308f674c0ee2bf8eeba6d
richard <richard@nextmovesoftware.com>
2022-09-19 14:49:00 +0100

    strip out FEMethod

--- openfe/setup/methods/__init__.py
+++ openfe/setup/methods/__init__.py
@@ -1,7 +1,6 @@
 # This code is part of OpenFE and is licensed under the MIT license.
 # For details, see https://github.com/OpenFreeEnergy/openfe
 
-from .base import FEMethod
 from .openmm import (
     RelativeLigandTransformSettings,
     RelativeLigandTransformResult,
