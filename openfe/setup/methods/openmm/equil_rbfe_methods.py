2d6018f7328e5d030aac6d9005d1d82eec26f771
richard <richard@nextmovesoftware.com>
2022-09-20 14:21:40 +0100

    fix positions being set as openff unit Quantitys in run()

--- openfe/setup/methods/openmm/equil_rbfe_methods.py
+++ openfe/setup/methods/openmm/equil_rbfe_methods.py
@@ -834,12 +834,12 @@ class RelativeLigandTransformUnit(gufe.ProtocolUnit):
                                            pdbfile._openmm_pos)
             stateA_modeller.add(
                 stateA_openff_ligand.to_topology().to_openmm(),
-                stateA_openff_ligand.conformers[0],
+                to_openmm(stateA_openff_ligand.conformers[0]),
             )
         else:
             stateA_modeller = app.Modeller(
                 stateA_openff_ligand.to_topology().to_openmm(),
-                stateA_openff_ligand.conformers[0],
+                to_openmm(stateA_openff_ligand.conformers[0]),
             )
 
         # 4. Solvate the complex in a `concentration` mM cubic water box with
