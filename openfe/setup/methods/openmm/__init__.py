91ce339a3aa59c017d1bac26c0b929145e410dca
richard <richardjgowers@gmail.com>
2022-10-28 18:41:47 +0100

    move Protocols from setup to new protocols module

--- openfe/setup/methods/openmm/__init__.py
+++
@@ -1,9 +0,0 @@
-# This code is part of OpenFE and is licensed under the MIT license.
-# For details, see https://github.com/OpenFreeEnergy/openfe
-
-from .equil_rbfe_methods import (
-    RelativeLigandTransform,
-    RelativeLigandTransformSettings,
-    RelativeLigandTransformResult,
-    RelativeLigandTransformUnit,
-)
