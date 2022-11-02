ec1b0cfc5c67b64359d09cbc8bd9ac8ff2949dc6
David W.H. Swenson <david.swenson@omsf.io>
2022-11-02 14:14:38 -0500

    convert from xUnit

--- openfecli/tests/test_cli.py
+++ openfecli/tests/test_cli.py
@@ -5,11 +5,11 @@ import click.testing
 from openfecli.cli import OpenFECLI, main
 from openfecli.plugins import OFECommandPlugin
 
+@pytest.fixture
+def cli():
+    return OpenFECLI()
 
 class TestCLI:
-    def setup(self):
-        self.cli = OpenFECLI()
-
     def test_invoke(self):
         runner = click.testing.CliRunner()
         with runner.isolated_filesystem():
@@ -19,19 +19,19 @@ class TestCLI:
             assert result.exit_code == 0
             assert "Usage: openfe" in result.output
 
-    def test_command_sections(self):
+    def test_command_sections(self, cli):
         # This test does not ensure the order of the sections, and does not
         # prevent other sections from being added later. It only ensures
         # that the main 4 sections continue to exist.
         included = ["Setup", "Simulation", "Orchestration", "Analysis"]
         for sec in included:
-            assert sec in self.cli.COMMAND_SECTIONS
+            assert sec in cli.COMMAND_SECTIONS
 
-    def test_get_installed_plugins(self):
+    def test_get_installed_plugins(self, cli):
         # Test that we correctly load some plugins. This test only ensures
         # that some plugins are loaded; it currently does nothing to ensure
         # the identity of the specific plugins.
-        plugins = self.cli.get_installed_plugins()
+        plugins = cli.get_installed_plugins()
         for plugin in plugins:
             assert isinstance(plugin, OFECommandPlugin)
 
