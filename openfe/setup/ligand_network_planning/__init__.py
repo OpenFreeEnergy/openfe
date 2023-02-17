5e49b93dfc1527fa12210e928b6da259511e734a
bries <benjamin-ries@outlook.com>
2023-02-17 21:04:58 +0100

    fixing network changes..

--- openfe/setup/ligand_network_planning/__init__.py
+++ openfe/setup/ligand_network_planning/__init__.py
@@ -1,6 +1,6 @@
 # This code is part of OpenFE and is licensed under the MIT license.
 # For details, see https://github.com/OpenFreeEnergy/openfe
 
-from kartograf.networks.mst_networker import generate_minimal_spanning_graph
+from kartograf.networks.mst_networker import generate_minimal_spanning_network
 from kartograf.networks.radial_networker import generate_radial_network
 
