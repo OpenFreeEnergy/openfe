f7c378902f82dbb1d9cf33d71a2a637852f0af63
riesben <benjamin-ries@outlook.com>
2023-03-08 18:04:19 +0100

    drafting a future structure

--- openfe/setup/campaigners/_abstract_campaigner.py
+++ openfe/setup/campaigners/_abstract_campaigner.py
@@ -4,8 +4,8 @@ from gufe import Component, AlchemicalNetwork
 
 class _abstract_campaigner():
 
-    def __call__(self, components:Iterable[Component])->AlchemicalNetwork:
+    def __call__(self, components: Iterable[Component]) -> AlchemicalNetwork:
         raise NotImplementedError()
 
-    def _update(self, alchemical_network:AlchemicalNetwork)->AlchemicalNetwork:
+    def _update(self, alchemical_network: AlchemicalNetwork) -> AlchemicalNetwork:
         raise NotImplementedError()
