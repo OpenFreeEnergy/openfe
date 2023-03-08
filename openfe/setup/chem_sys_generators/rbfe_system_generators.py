069e590d8822565a4d70e9493cd0cea6ed57e6ce
riesben <benjamin-ries@outlook.com>
2023-03-08 18:04:19 +0100

    drafting a future structure

---
+++ openfe/setup/chem_sys_generators/rbfe_system_generators.py
@@ -0,0 +1,35 @@
+from _abstract_chem_sys_generator import _abstract_chem_sys_generator
+
+from typing import Iterable
+from gufe import SmallMoleculeComponent, ProteinComponent, SolventComponent, ChemicalSystem
+
+
+class rbfe_system_generator(_abstract_chem_sys_generator):
+
+    def __init__(self, solvent: SolventComponent = None, protein: ProteinComponent = None, do_vacuum: bool = False):
+        self.solvent = solvent
+        self.protein = protein
+        self.do_vacuum = do_vacuum
+
+        if (solvent is None and protein is None and not do_vacuum):
+            raise ValueError("you need to provide any transformation possibility")
+
+    def __call__(self, component: SmallMoleculeComponent) -> Iterable[ChemicalSystem]:
+        chemical_systems = []
+        if (self.solvent is not None):
+            chem_sys = ChemicalSystem(components={"compA": component,
+                                                  "solvent": self.solvent})
+            chemical_systems.append(chem_sys, name=component.name + "_solvent")
+
+        if (self.solvent is not None and self.protein is not None):
+            components = {"compA": component, "receptor": self.protein}
+            if (self.solvent is not None):
+                components.update({"solvent": self.solvent})
+            chem_sys = ChemicalSystem(components=components, name=component.name + "_receptor")
+            chemical_systems.append(chem_sys)
+
+        if (self.do_vacuum):
+            chem_sys = ChemicalSystem(components={"compA": component})
+            chemical_systems.append(chem_sys, name=component.name + "_vacuum")
+
+        return chemical_systems
