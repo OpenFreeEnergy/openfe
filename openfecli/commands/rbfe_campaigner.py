8b992e98858abcd1101e7045906b190b36cac386
riesben <benjamin-ries@outlook.com>
2023-03-08 11:54:58 +0100

    discussions in last powerhour :) still a draft ;)

---
+++ openfecli/commands/rbfe_campaigner.py
@@ -0,0 +1,10 @@
+
+
+"""
+Here I want to build the cmd tool for easy campaigner with RBFE. The output should be runnable with quickrun directly!
+    So user experience would be:
+        easy_campaign -l sdf_dir -p receptor.pdb -> Alchem network
+        quickrun -i alchem_network
+
+"""
+
