02b4c70374f59b2d6e78fb5b24229f48bc840911
riesben <benjamin-ries@outlook.com>
2023-03-08 18:48:06 +0100

    drafting a future structure

--- openfe/setup/chem_sys_generators/_abstract_chem_sys_generator.py
+++ openfe/setup/chem_sys_generators/_abstract_chem_sys_generator.py
@@ -1,3 +1,6 @@
+# This code is part of OpenFE and is licensed under the MIT license.
+# For details, see https://github.com/OpenFreeEnergy/openfe
+
 from typing import Iterable
 from gufe import ChemicalSystem
 class _abstract_chem_sys_generator()
