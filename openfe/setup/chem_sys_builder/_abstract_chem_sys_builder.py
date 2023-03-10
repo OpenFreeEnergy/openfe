241d9283c9838828eb6bab4d0f2738f19a94469c
riesben <benjamin-ries@outlook.com>
2023-03-10 12:15:22 +0100

    refactor

--- openfe/setup/chem_sys_builder/_abstract_chem_sys_builder.py
+++ openfe/setup/chem_sys_builder/_abstract_chem_sys_builder.py
@@ -3,6 +3,6 @@
 
 from typing import Iterable
 from gufe import ChemicalSystem
-class _abstract_chem_sys_generator()
+class _abstract_chem_sys_builder()
 
     def __call__(self, *args, *kwargs) -> Iterable[ChemicalSystem]:
