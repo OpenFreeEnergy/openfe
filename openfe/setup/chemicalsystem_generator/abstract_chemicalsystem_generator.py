66c7eeaa45ffaf105efe55cb8b782ff0e368520b
bries <benjamin-ries@outlook.com>
2023-03-14 21:12:49 +0100

    making system generators

--- openfe/setup/chemicalsystem_generator/abstract_chemicalsystem_generator.py
+++ openfe/setup/chemicalsystem_generator/abstract_chemicalsystem_generator.py
@@ -3,6 +3,8 @@
 
 from typing import Iterable
 from gufe import ChemicalSystem
-class _abstract_chem_sys_builder()
 
-    def __call__(self, *args, *kwargs) -> Iterable[ChemicalSystem]:
+class Abstract_ChemicalSystem_generator():
+    
+    def __call__(self, *args, **kwargs) -> Iterable[ChemicalSystem]:
+        return NotImplementedError( )
