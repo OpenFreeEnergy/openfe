0300cbff45899876e46e0299b32ce0b6c3cb05ad
riesben <benjamin-ries@outlook.com>
2023-03-15 15:15:49 +0100

    adding ABC

--- openfe/setup/alchemical_network_planner/abstract_alchemical_network_planner.py
+++ openfe/setup/alchemical_network_planner/abstract_alchemical_network_planner.py
@@ -1,10 +1,13 @@
 # This code is part of OpenFE and is licensed under the MIT license.
 # For details, see https://github.com/OpenFreeEnergy/openfe
 
+import abc
 from typing import Iterable
 from gufe import Component, AlchemicalNetwork
 
 
-class AbstractAlchemicalNetworkPlanner:
+class AbstractAlchemicalNetworkPlanner(abc.ABC):
+
+    @abc.abstractmethod
     def __call__(self, components: Iterable[Component]) -> AlchemicalNetwork:
         raise NotImplementedError()
--- openfe/setup/chemicalsystem_generator/abstract_chemicalsystem_generator.py
+++ openfe/setup/chemicalsystem_generator/abstract_chemicalsystem_generator.py
@@ -1,10 +1,13 @@
 # This code is part of OpenFE and is licensed under the MIT license.
 # For details, see https://github.com/OpenFreeEnergy/openfe
+import abc
 
 from typing import Iterable
 from gufe import ChemicalSystem
 
 
-class AbstractChemicalSystemGenerator:
+class AbstractChemicalSystemGenerator(abc.ABC):
+
+    @abc.abstractmethod
     def __call__(self, *args, **kwargs) -> Iterable[ChemicalSystem]:
         raise NotImplementedError()
