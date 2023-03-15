048c6e83ddfa8723e8f94ff171a3d76dea21e3d1
riesben <benjamin-ries@outlook.com>
2023-03-15 17:19:52 +0100

    update cmd

--- openfecli/commands/plan_rbfe_campaign.py
+++ openfecli/commands/plan_rbfe_campaign.py
@@ -15,62 +15,87 @@ from openfecli.utils import write
 from openfecli import OFECommandPlugin
 from openfecli.parameters import MOL, PROTEIN, MAPPER, OUTPUT_FILE_AND_EXT
 
+
 @click.command(
-    'plan_rbfe',
+    'plan_rbfe_campaign',
     short_help="Run a planning session, saved as a JSON file"
 )
-@MOL.parameter(multiple=True, required=True,
-               help=MOL.kwargs['help']+" Any number of sdf paths.")
-@PROTEIN.parameter(multiple=False, required=False, default=None,
-               help=MOL.kwargs['help'])
-@OUTPUT_FILE_AND_EXT.parameter(
-    help=OUTPUT_FILE_AND_EXT.kwargs['help'] + " (JSON format)"
+@PROTEIN.parameter(
+    multiple=False, required=True, default=None, help=MOL.kwargs["help"]
 )
-@MAPPER.parameter(required=False, default="lomap")
-def plan_rbfe_campaign(mol:str, receptor_pdb_path:str, output:str, mapper:str):
+@OUTPUT_FILE_AND_EXT.parameter(
+    help=OUTPUT_FILE_AND_EXT.kwargs["help"] + " (JSON format)"
+)
+@MAPPER.parameter(required=False, default="LomapAtomMapper")
+def plan_rbfe_campaign(mol: str, receptor_pdb_path: str, output: str, mapper: str):
 
     write("Parsing in Files: ")
     write("\tGot input: ")
-    from gufe import SolventComponent
+
     small_molecules = [MOL.get(m) for m in mol]
-    write("\t\tSmall Molecules: "+" ".join([str(sm) for sm in small_molecule_paths]))
+    write("\t\tSmall Molecules: " + " ".join([str(sm) for sm in small_molecules]))
 
-    if(protein is not None):
-        protein = PROTEIN.get(receptor_pdb_path)
-    write("\t\tProtein: "+str(protein))
+    protein = PROTEIN.get(receptor_pdb_path)
+    write("\t\tProtein: " + str(protein))
 
+    from gufe import SolventComponent
     solvent = SolventComponent()
-    write("\t\tSolvent: "+str(solvent))
+    write("\t\tSolvent: " + str(solvent))
     write("")
 
     write("Using Options:")
     mapper_obj = MAPPER.get(mapper)()
-    write("\tMapper: "+str(mapper_obj))
+    write("\tMapper: " + str(mapper_obj))
 
-    from openfe.setup.atom_mapping.lomap_scorers import default_lomap_score #write nice parameter
-    mapping_scorers = [default_lomap_score]
-    write("\tMapping Scorer: "+str(mapping_scorers))
+    from openfe.setup.atom_mapping.lomap_scorers import (
+        default_lomap_score,
+    )  # write nice parameter
 
-    from openfe.setup.ligand_network_planning import minimal_spanning_graph #write nice parameter
-    networker = minimal_spanning_graph
-    write("\tNetworker: "+str(networker))
+    mapping_scorer = default_lomap_score
+    write("\tMapping Scorer: " + str(mapping_scorer))
+
+    from openfe.setup.ligand_network_planning import (
+        generate_minimal_spanning_network,
+    )  # write nice parameter
+
+    ligand_network_planner = generate_minimal_spanning_network
+    write("\tNetworker: " + str(ligand_network_planner))
     write("")
 
     write("Planning RBFE-Campaign:")
-    from openfe.setup.campaigners.easy_campaigner import rbfe_campaigner
-    campaigner = rbfe_campaigner(mapper=mapper_obj,
-                                 mapping_scorers=mapping_scorers,
-                                 networker=networker)
-    alchemical_network = campaigner(ligands=small_molecules, solvent=solvent, receptor=protein)
+    alchemical_network = plan_rbfe_campaign_main(
+        mapper=mapper_obj,
+        mapping_scorer=mapping_scorer,
+        ligand_network_planner=ligand_network_planner,
+        small_molecules=small_molecules,
+        solvent=solvent,
+        protein=protein,
+    )
 
-    write("Saving to: "+str(output))
+    write("Saving to: " + str(output))
     an_dict = alchemical_network.to_dict()
-    file, ext = OUTPUT_FILE_AND_EXT.get(output) #check ext.
-    json.dump(an_dict, open(file+"."+ext, "w"))
+    file, ext = OUTPUT_FILE_AND_EXT.get(output)  # check ext.
+    json.dump(an_dict, open(file + "." + ext, "w"))
+
+
+def plan_rbfe_campaign_main(
+    mapper, mapping_scorer, ligand_network_planner, small_molecules, solvent, protein
+):
+    from openfe.setup.alchemical_network_planner.easy_alchemical_network_planner import (
+        RBFEAlchemicalNetworkPlanner,
+    )
+
+    campaigner = RBFEAlchemicalNetworkPlanner(
+        mapper=mapper,
+        mapping_scorer=mapping_scorer,
+        ligand_network_planner=ligand_network_planner,
+    )
+    alchemical_network = campaigner(
+        ligands=small_molecules, solvent=solvent, receptor=protein
+    )
+    return alchemical_network
 
 
 PLUGIN = OFECommandPlugin(
-    command=plan_rbfe_campaign,
-    section="Setup",
-    requires_ofe=(0, 0, 1)
+    command=plan_rbfe_campaign, section="Setup", requires_ofe=(0, 3)
 )
