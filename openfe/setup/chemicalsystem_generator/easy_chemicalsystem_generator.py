91138c2185675112bf322e6f8b224f25272d897b
riesben <benjamin-ries@outlook.com>
2023-03-15 15:15:49 +0100

    adding ABC

--- openfe/setup/chemicalsystem_generator/easy_chemicalsystem_generator.py
+++ openfe/setup/chemicalsystem_generator/easy_chemicalsystem_generator.py
@@ -2,8 +2,9 @@
 # For details, see https://github.com/OpenFreeEnergy/openfe
 
 from .abstract_chemicalsystem_generator import AbstractChemicalSystemGenerator
-
+from enum import Enum
 from typing import Iterable
+
 from gufe import (
     SmallMoleculeComponent,
     ProteinComponent,
@@ -11,6 +12,13 @@ from gufe import (
     ChemicalSystem,
 )
 
+# Todo: connect to protocols - use this for labels?
+class RFEChemicalSystemComponentLabels(str, Enum):
+    PROTEIN = 'protein'
+    LIGAND = 'ligand'
+    SOLVENT= "sovlent"
+
+component_labels =RFEChemicalSystemComponentLabels
 
 class EasyChemicalSystemGenerator(AbstractChemicalSystemGenerator):
     def __init__(
@@ -29,13 +37,13 @@ class EasyChemicalSystemGenerator(AbstractChemicalSystemGenerator):
     def __call__(self, component: SmallMoleculeComponent) -> Iterable[ChemicalSystem]:
         if self.do_vacuum:
             chem_sys = ChemicalSystem(
-                components={"compA": component}, name=component.name + "_vacuum"
+                components={component_labels.LIGAND: component}, name=component.name + "_vacuum"
             )
             yield chem_sys
 
         if self.solvent is not None:
             chem_sys = ChemicalSystem(
-                components={"compA": component, "solvent": self.solvent},
+                components={component_labels.LIGAND: component, component_labels.SOLVENT: self.solvent},
                 name=component.name + "_solvent",
             )
             yield chem_sys
@@ -43,9 +51,9 @@ class EasyChemicalSystemGenerator(AbstractChemicalSystemGenerator):
         print(self.protein)
 
         if self.protein is not None:
-            components = {"compA": component, "receptor": self.protein}
+            components = {component_labels.LIGAND: component, component_labels.PROTEIN: self.protein}
             if self.solvent is not None:
-                components.update({"solvent": self.solvent})
+                components.update({component_labels.SOLVENT: self.solvent})
             chem_sys = ChemicalSystem(
                 components=components, name=component.name + "_receptor"
             )
--- openfe/setup/transformation_factory/abstract_transformation_factory.py
+++ openfe/setup/transformation_factory/abstract_transformation_factory.py
@@ -1,10 +1,11 @@
 # This code is part of OpenFE and is licensed under the MIT license.
 # For details, see https://github.com/OpenFreeEnergy/openfe
 
+import abc
 from gufe import AlchemicalNetwork
 
 
-class AbstractTransformerFactory:
+class AbstractTransformerFactory(abc.ABC):
     """
     This class type is thought to be the part that transforms identified edges/transformations for chemical systems of an FE-campaign to an Alchemical
     Network.
@@ -13,5 +14,6 @@ class AbstractTransformerFactory:
     where one transformation gets a protocols assigned from a set of protocols, depending on certain metrics.
     """
 
+    @abc.abstractmethod
     def __call__(self, *args, **kwargs) -> AlchemicalNetwork:
         raise NotImplementedError()
