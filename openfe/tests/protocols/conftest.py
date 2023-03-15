86835cfbaca1f212832f274b522b83eab13199ac
richard gowers <richardjgowers@gmail.com>
2023-03-15 20:59:52 +0000

    tests: move fixtures out of test_openmm_equil_protocols

---
+++ openfe/tests/protocols/conftest.py
@@ -0,0 +1,73 @@
+# This code is part of OpenFE and is licensed under the MIT license.
+# For details, see https://github.com/OpenFreeEnergy/openfe
+import pytest
+import openfe
+from openff.units import unit
+
+
+@pytest.fixture
+def benzene_vacuum_system(benzene_modifications):
+    return openfe.ChemicalSystem(
+        {'ligand': benzene_modifications['benzene']},
+    )
+
+
+@pytest.fixture
+def benzene_system(benzene_modifications):
+    return openfe.ChemicalSystem(
+        {'ligand': benzene_modifications['benzene'],
+         'solvent': openfe.SolventComponent(
+             positive_ion='Na', negative_ion='Cl',
+             ion_concentration=0.15 * unit.molar)
+        },
+    )
+
+
+@pytest.fixture
+def benzene_complex_system(benzene_modifications, T4_protein_component):
+    return openfe.ChemicalSystem(
+        {'ligand': benzene_modifications['benzene'],
+         'solvent': openfe.SolventComponent(
+             positive_ion='Na', negative_ion='Cl',
+             ion_concentration=0.15 * unit.molar),
+         'protein': T4_protein_component,}
+    )
+
+
+@pytest.fixture
+def toluene_vacuum_system(benzene_modifications):
+    return openfe.ChemicalSystem(
+        {'ligand': benzene_modifications['toluene']},
+    )
+
+
+@pytest.fixture
+def toluene_system(benzene_modifications):
+    return openfe.ChemicalSystem(
+        {'ligand': benzene_modifications['toluene'],
+         'solvent': openfe.SolventComponent(
+             positive_ion='Na', negative_ion='Cl',
+             ion_concentration=0.15 * unit.molar),
+        },
+    )
+
+
+@pytest.fixture
+def toluene_complex_system(benzene_modifications, T4_protein_component):
+    return openfe.ChemicalSystem(
+        {'ligand': benzene_modifications['toluene'],
+         'solvent': openfe.SolventComponent(
+             positive_ion='Na', negative_ion='Cl',
+             ion_concentration=0.15 * unit.molar),
+         'protein': T4_protein_component,}
+    )
+
+
+@pytest.fixture
+def benzene_to_toluene_mapping(benzene_modifications):
+    mapper = openfe.setup.LomapAtomMapper(element_change=False)
+
+    molA = benzene_modifications['benzene']
+    molB = benzene_modifications['toluene']
+
+    return next(mapper.suggest_mappings(molA, molB))
