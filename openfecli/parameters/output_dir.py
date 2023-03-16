9e713088da0e74e6716c96c5aad496f553230d1c
riesben <benjamin-ries@outlook.com>
2023-03-16 15:12:41 +0100

    rhfe and rbfe planner running first time

--- openfecli/parameters/output_dir.py
+++ openfecli/parameters/output_dir.py
@@ -3,10 +3,9 @@ import pathlib
 from plugcli.params import MultiStrategyGetter, Option, NOT_PARSED
 
 
-def get_file_and_extension(user_input, context):
-    file = user_input
-    ext = file.name.split('.')[-1] if file else None
-    return file, ext
+def get_dir(user_input, context):
+    dir_path = user_input
+    return dir_path
 
 
 def ensure_file_does_not_exist(ctx, param, value):
@@ -15,9 +14,9 @@ def ensure_file_does_not_exist(ctx, param, value):
     return value
 
 
-OUTPUT_FILE_AND_EXT = Option(
-    "-o", "--output",
-    help="output file",
-    getter=get_file_and_extension,
-    type=click.File(mode='wb'),
+OUTPUT_DIR = Option(
+    "-od", "--output_dir",
+    help="output_dir",
+    getter=get_dir,
+    type=click.Path(file_okay=False, resolve_path=True),
 )
