a658ca0279e19e9c109a178b230489382066daa0
riesben <benjamin-ries@outlook.com>
2023-03-16 13:12:34 +0100

    fireup cli tool :)

--- openfecli/parameters/protein.py
+++ openfecli/parameters/protein.py
@@ -8,7 +8,7 @@ def _load_protein_from_pdb(user_input, context):
     if '.pdb' not in str(user_input):  # this silences some stderr spam
         return NOT_PARSED
 
-    from openfe.setup import ProteinComponent
+    from gufe import ProteinComponent
     try:
         return ProteinComponent.from_pdb_file(user_input)
     except ValueError:  # any exception should try other strategies
@@ -18,7 +18,7 @@ def _load_protein_from_pdbx(user_input, context):
     if '.pdbx' not in str(user_input):  # this silences some stderr spam
         return NOT_PARSED
 
-    from openfe.setup import ProteinComponent
+    from gufe import ProteinComponent
     try:
         return ProteinComponent.from_pdbx_file(user_input)
     except ValueError:  # any exception should try other strategies
@@ -34,7 +34,7 @@ get_molecule = MultiStrategyGetter(
 )
 
 PROTEIN = Option(
-    "--protein",
+    "-p", "--protein",
     help=("ProteinComponent. Can be provided as an PDB file or as a PDBx/mmCIF file. "
           " string."),
     getter=get_molecule
