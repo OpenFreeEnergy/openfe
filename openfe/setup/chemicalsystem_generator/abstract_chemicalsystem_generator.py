2141fdea1ad25fad5cb4b466b9a09343686e7ccf
bries <benjamin-ries@outlook.com>
2023-03-24 10:23:23 +0100

    merging transformation factory into alchemical network planner

--- openfe/setup/chemicalsystem_generator/abstract_chemicalsystem_generator.py
+++ openfe/setup/chemicalsystem_generator/abstract_chemicalsystem_generator.py
@@ -1,9 +1,16 @@
 # This code is part of OpenFE and is licensed under the MIT license.
 # For details, see https://github.com/OpenFreeEnergy/openfe
 import abc
+from enum import Enum
 
 from typing import Iterable
 from gufe import ChemicalSystem
+
+# Todo: connect to protocols - use this for labels?
+class RFEComponentLabels(str, Enum):
+    PROTEIN = "protein"
+    LIGAND = "ligand"
+    SOLVENT = "solvent"
 
 
 class AbstractChemicalSystemGenerator(abc.ABC):
