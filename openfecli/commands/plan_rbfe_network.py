662ece0a01f69670e69ac19f206023d939d8f885
bries <benjamin-ries@outlook.com>
2023-03-24 19:43:42 +0100

    adding cli test, only need to fix one last

--- openfecli/commands/plan_rbfe_network.py
+++ openfecli/commands/plan_rbfe_network.py
@@ -3,7 +3,7 @@
 
 
 """
-Here I want to build the cmd tool for easy campaigner with RHFE. The output should be runnable with quickrun directly!
+Here I want to build the cmd tool for easy campaigner with RBFE. The output should be runnable with quickrun directly!
     So user experience would be:
         easy_campaign -l sdf_dir -p receptor.pdb -> Alchem network
         quickrun -i alchem_network
@@ -16,33 +16,32 @@ import click
 from typing import List
 from openfecli.utils import write
 from openfecli import OFECommandPlugin
-from openfecli.parameters import MOL_DIR, MAPPER, OUTPUT_DIR
+from openfecli.parameters import MOL_DIR, PROTEIN, MAPPER, OUTPUT_DIR
 
-# Todo: Make Params exchangeable - Lomap, Kartograf, etc.
 
-def plan_rhfe_network_main(
-    mapper, mapping_scorer, ligand_network_planner, small_molecules, solvent
+def plan_rbfe_network_main(
+    mapper, mapping_scorer, ligand_network_planner, small_molecules, solvent, protein
 ):
-    from openfe.setup.alchemical_network_planner.easy_alchemical_network_planner import (
-        RHFEAlchemicalNetworkPlanner,
+    from openfe.setup.alchemical_network_planner.relative_alchemical_network_planner import (
+        RBFEAlchemicalNetworkPlanner,
     )
 
-    campaigner = RHFEAlchemicalNetworkPlanner(
-        mapper=mapper,
+    network_planner = RBFEAlchemicalNetworkPlanner(
+        mappers=[mapper],
         mapping_scorer=mapping_scorer,
         ligand_network_planner=ligand_network_planner,
     )
-    alchemical_network = campaigner(
-        ligands=small_molecules, solvent=solvent
+    alchemical_network = network_planner(
+        ligands=small_molecules, solvent=solvent, protein=protein
     )
+    
     return alchemical_network
 
-def plan_rhfe_network_output(alchemical_network, output):
-    #Todo: this is an uggly peace of output code. it does not recognize overwriting! fix!
+def plan_rbfe_network_output(alchemical_network, output):
+    #Todo: this is an uggly peace of output code. it does not recognize overwriting! fix
     an_dict = alchemical_network.to_dict()
 
     folder_path = output
-    print(folder_path)
     base_name= os.path.basename(folder_path)
 
     if not os.path.isdir(folder_path):
@@ -63,17 +62,20 @@ def plan_rhfe_network_output(alchemical_network, output):
 
 
 @click.command(
-    "plan-rhfe-network", short_help="Run a planning session for relative hydration free energies, saved as a JSON file"
+    "plan-rbfe-network", short_help="Run a planning session for relative binding free energies, saved as a JSON file"
 )
 @MOL_DIR.parameter(
     required=True, help=MOL_DIR.kwargs["help"] + " Any number of sdf paths."
+)
+@PROTEIN.parameter(
+    multiple=False, required=True, default=None, help=PROTEIN.kwargs["help"]
 )
 @OUTPUT_DIR.parameter(
     help=OUTPUT_DIR.kwargs["help"] + " ",
     default="alchemicalNetwork"
 )
 @MAPPER.parameter(required=False, default="LomapAtomMapper")
-def plan_rhfe_network(mol_dir: List[str], output_dir: str, mapper: str):
+def plan_rbfe_network(mol_dir: List[str], protein: str, output_dir: str, mapper: str):
 
     # INPUT
     write("Parsing in Files: ")
@@ -81,6 +83,9 @@ def plan_rhfe_network(mol_dir: List[str], output_dir: str, mapper: str):
 
     small_molecules = MOL_DIR.get(mol_dir)
     write("\t\tSmall Molecules: " + " ".join([str(sm) for sm in small_molecules]))
+
+    protein = PROTEIN.get(protein)
+    write("\t\tProtein: " + str(protein))
 
     from gufe import SolventComponent
     solvent = SolventComponent()
@@ -93,14 +98,14 @@ def plan_rhfe_network(mol_dir: List[str], output_dir: str, mapper: str):
 
     from openfe.setup.atom_mapping.lomap_scorers import (
         default_lomap_score,
-    )  # write nice parameter
+    )  # TODO:  write nice parameter
 
     mapping_scorer = default_lomap_score
     write("\tMapping Scorer: " + str(mapping_scorer))
 
     from openfe.setup.ligand_network_planning import (
         generate_minimal_spanning_network
-    )  # write nice parameter
+    )  # TODO:  write nice parameter
 
     ligand_network_planner = generate_minimal_spanning_network
     write("\tNetworker: " + str(ligand_network_planner))
@@ -108,23 +113,25 @@ def plan_rhfe_network(mol_dir: List[str], output_dir: str, mapper: str):
 
     # DO
     write("Planning RBFE-Campaign:")
-    alchemical_network = plan_rhfe_network_main(
+    alchemical_network = plan_rbfe_network_main(
         mapper=mapper_obj,
         mapping_scorer=mapping_scorer,
         ligand_network_planner=ligand_network_planner,
         small_molecules=small_molecules,
         solvent=solvent,
+        protein=protein,
     )
     write("\tDone")
     write("")
 
     # OUTPUT
     write("Output:")
-    write("\tSaving to: " + output_dir) #Todo: remove replace
-    plan_rhfe_network_output(alchemical_network=alchemical_network, output=OUTPUT_DIR.get(output_dir))
+    write("\tSaving to: " + str(output_dir))
+    plan_rbfe_network_output(alchemical_network=alchemical_network, output=OUTPUT_DIR.get(output_dir))
 
 
 
 PLUGIN = OFECommandPlugin(
-    command=plan_rhfe_network, section="Setup", requires_ofe=(0, 3)
+    command=plan_rbfe_network, section="Setup", requires_ofe=(0, 3)
 )
+
