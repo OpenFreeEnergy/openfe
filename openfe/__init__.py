6795521b6776acbc885e542efddc88c27db912c3
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2023-04-03 10:32:35 -0700

    switch to getting version using package metadata

--- openfe/__init__.py
+++ openfe/__init__.py
@@ -21,5 +21,5 @@ from .setup import (
 from . import orchestration
 from . import analysis
 
-from openfecli import _version
-__version__ = _version.__version__
+from importlib.metadata import version
+__version__ = version("openfe")
