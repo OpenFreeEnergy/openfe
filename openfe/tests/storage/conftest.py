276044e586c79860fa53baa6c3fa79c4a62ed1a4
IAlibay <ialibay@mdanalysis.org>
2023-04-10 20:46:51 +0100

    more imports

--- openfe/tests/storage/conftest.py
+++ openfe/tests/storage/conftest.py
@@ -1,4 +1,4 @@
 from gufe.tests.conftest import (
     absolute_transformation, complex_equilibrium,
-    benzene_variants_star_map
+    benzene_variants_star_map, solvated_ligand, solvated_complex, benzene
 )
