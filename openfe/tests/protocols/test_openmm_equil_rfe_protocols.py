9ad5c18aae901d35a20e59fd36973da60f4dc75c
IAlibay <ialibay@mdanalysis.org>
2023-04-11 16:19:24 +0100

    skip all complex dry runs

--- openfe/tests/protocols/test_openmm_equil_rfe_protocols.py
+++ openfe/tests/protocols/test_openmm_equil_rfe_protocols.py
@@ -133,6 +133,8 @@ def test_dry_run_ligand(benzene_system, toluene_system,
         assert sampler.is_periodic
 
 
+@pytest.mark.slow
+@pytest.mark.parametrize('method', ['repex', 'sams', 'independent'])
 def test_dry_run_complex(benzene_complex_system, toluene_complex_system,
                          benzene_to_toluene_mapping, tmpdir):
     # this will be very time consuming
