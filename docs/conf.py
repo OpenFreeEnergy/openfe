95a73e316f10bd8af73aa3226af4cb5f5ad23d58
richard gowers <richardjgowers@gmail.com>
2023-04-26 14:52:11 +0100

    use both class docstring and init docstring in autoclass

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,8 @@ extensions = [
     'sphinx_toolbox.collapse',
 ]
 
+autoclass_content = 'both'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
