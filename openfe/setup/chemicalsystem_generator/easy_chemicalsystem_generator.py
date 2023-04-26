da4962b55e459462582cde08cacae8341400628c
richard gowers <richardjgowers@gmail.com>
2023-04-26 14:51:56 +0100

    fixup easychemicalsystem docs
    was giving warning about indentation

--- openfe/setup/chemicalsystem_generator/easy_chemicalsystem_generator.py
+++ openfe/setup/chemicalsystem_generator/easy_chemicalsystem_generator.py
@@ -23,7 +23,8 @@ class EasyChemicalSystemGenerator(AbstractChemicalSystemGenerator):
         do_vacuum: bool = False,
     ):
         """This class is a easy generator class, for generating chemical systems with a focus on a given SmallMoleculeComponent.
-        depending on which parameters are given, the following systems will be generated in order:
+        depending on which parameters are given, the following systems will be generated in order::
+
             vacuum -> solvent -> protein
 
         Parameters
