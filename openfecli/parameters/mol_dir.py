2e12ab6f85ac0bed4225caf2916319a62dab4e1f
David W.H. Swenson <david.swenson@omsf.io>
2023-04-28 15:35:31 -0500

    little cleanup

--- openfecli/parameters/mol_dir.py
+++ openfecli/parameters/mol_dir.py
@@ -1,10 +1,58 @@
 # This code is part of OpenFE and is licensed under the MIT license.
 # For details, see https://github.com/OpenFreeEnergy/openfe
+import click
 import glob
 import itertools
 
 from plugcli.params import MultiStrategyGetter, Option, NOT_PARSED
 
+# MOVE TO GUFE ####################################################
+def _smcs_from_sdf(sdf):
+    from openfe import SmallMoleculeComponent
+    from rdkit import Chem
+    supp = Chem.SDMolSupplier(sdf)
+    mols = [SmallMoleculeComponent(mol) for mol in Chem.SDMolSupplier]
+
+def _smcs_from_mol2(mol2):
+    from openfe import SmallMoleculeComponent
+    from rdkit import Chem
+    rdmol = Chem.MolFromMol2File(mol2, removeHs=False)
+    return [SmallMoleculeComponent.from_rdkit(rdmol)]
+
+def load_molecules(file_or_directory):
+    """
+    Load SmallMoleculeComponents in the given file or directory.
+
+    This always returns a list. The input can be a directory, in which all
+    files ending with .sdf are loaded as SDF and all files ending in .mol2
+    are loaded as MOL2.
+
+    Parameters
+    ----------
+    file_or_directory : pathlib.Path
+
+    Returns
+    -------
+    list[SmallMoleculeComponent]
+    """
+    inp = file_or_directory  # for shorter lines
+    if inp.is_dir():
+        sdf_files = [f for f in inp.iterdir() if f.suffix.lower() == "sdf"]
+        mol2_files = [f for f in inp.iterdir()
+                      if f.suffix.lower() == "mol2"]
+    else:
+        sdf_files = [inp] if inp.suffix.lower() == "sdf" else []
+        mol2_files = [inp] if inp.suffix.lower() == "mol2" else []
+
+    sdf_mols = sum([_smcs_from_sdf(sdf) for sdf in sdf_files], [])
+    mol2_mols = sum([_smcs_from_mol2(mol2) for mol2 in mol2_files], [])
+
+    return sdf_mols + mol2_mols
+# END MOVE TO GUFE ################################################
+
+
+def molecule_getter(user_input, context):
+    return load_molecules(user_input)
 
 def _load_molecules_from_sdf(user_input, context):
     from rdkit import Chem
@@ -51,10 +99,11 @@ get_molecules = MultiStrategyGetter(
 
 MOL_DIR = Option(
     "-M",
-    "--mol-dir",
+    "--molecules",
+    type=click.Path(exists=True),
     help=(
-        "SmallMoleculeComponents from a folder. Folder needs to contain SDF/MOL2 files"
-        " string."
+        "A directory or file containing all molecules to be loaded, either"
+        " as a single SDF or multiple MOL2/SDFs."
     ),
-    getter=get_molecules,
+    getter=molecule_getter,
 )
