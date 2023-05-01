88b4190fde40080a5b28b94a2304105d417f6308
David W.H. Swenson <david.swenson@omsf.io>
2023-05-01 09:19:50 -0500

    Add expected behavior

--- openfecli/commands/test.py
+++ openfecli/commands/test.py
@@ -19,6 +19,10 @@ def test(long):
     minutes. If given the ``--long`` flag, this will include several tests
     that take significantly longer, but ensure that we're able to fully run
     in your environment.
+
+    A successful run will include tests that pass, skip, or "xfail". In many
+    terminals, these show as green or yellow. Warnings are not a concern.
+    However, You should not see anything that fails or errors (red).
     """
     old_env = dict(os.environ)
     os.environ["OFE_SLOW_TESTS"] = str(long)
