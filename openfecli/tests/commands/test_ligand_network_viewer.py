bd95cb1f2eb686abb65557d02bc4c72b55eca42c
David W.H. Swenson <david.swenson@omsf.io>
2023-05-03 17:47:21 -0500

    helps when the test actually runs

--- openfecli/tests/commands/test_ligand_network_viewer.py
+++ openfecli/tests/commands/test_ligand_network_viewer.py
@@ -1,16 +1,24 @@
 import pytest
+from unittest import mock
 from click.testing import CliRunner
 import importlib.resources
 import matplotlib
 
 from openfecli.commands.ligand_network_viewer import ligand_network_viewer
 
+@pytest.mark.filterwarnings("ignore:.*non-GUI backend")
 def test_ligand_network_viewer():
     # smoke test
     resource = importlib.resources.files('openfe.tests.data.serialization')
     ref = resource / "network_template.graphml"
     runner = CliRunner()
+
+    backend = matplotlib.get_backend()
+    matplotlib.use("ps")
+    loc = "openfe.utils.atommapping_network_plotting.matplotlib.use"
     with runner.isolated_filesystem():
-        # interestingly, this doesn't seem to require the patch to avoid
-        # launching a matplotlib window
-        runner.invoke(ligand_network_viewer, ref)
+        with mock.patch(loc, mock.Mock()):
+            result = runner.invoke(ligand_network_viewer, [str(ref)])
+            assert result.exit_code == 0
+
+    matplotlib.use(backend)
