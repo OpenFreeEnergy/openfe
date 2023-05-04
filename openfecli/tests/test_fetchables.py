754be5e55667b8202fe5bfa5f613066eb8df279a
David W.H. Swenson <david.swenson@omsf.io>
2023-05-04 13:14:27 -0500

    add test_fetchables

---
+++ openfecli/tests/test_fetchables.py
@@ -0,0 +1,25 @@
+import pytest
+from click.testing import CliRunner
+from .conftest import HAS_INTERNET
+import pathlib
+
+from openfecli.fetching import FetchablePlugin
+
+from openfecli.fetchables import RHFE_TUTORIAL
+
+def fetchable_test(fetchable):
+    """Unit test to ensure that a given FetchablePlugin works"""
+    assert isinstance(fetchable, FetchablePlugin)
+    expected_paths = [pathlib.Path(f) for f in fetchable.filenames]
+    runner = CliRunner()
+    if fetchable.fetcher.REQUIRES_INTERNET and not HAS_INTERNET:
+        pytest.skip("Internet seems to be unavailable")
+    with runner.isolated_filesystem():
+        result = runner.invoke(fetchable.command, ['-d' 'output-dir'])
+        assert result.exit_code == 0
+        for path in expected_paths:
+            assert (pathlib.Path("output-dir") / path).exists()
+
+
+def test_rhfe_tutorial():
+    fetchable_test(RHFE_TUTORIAL)
