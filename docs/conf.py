fab5a65b8a243fb9af6d916a36eebe879d4e4f5b
IAlibay <ialibay@mdanalysis.org>
2023-05-06 01:26:58 +0100

    try adding logo

--- docs/conf.py
+++ docs/conf.py
@@ -71,6 +71,8 @@ autodoc_mock_imports = ['openff.models',
 #
 html_theme = 'pydata_sphinx_theme'
 
+html_logo = "_static/Squaredcircle.svg"
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
