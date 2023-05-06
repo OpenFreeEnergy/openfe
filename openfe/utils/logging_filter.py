7e2c1794e2c9206ab9c1583832d43ca32bb97826
David W.H. Swenson <david.swenson@omsf.io>
2023-05-06 13:33:27 -0500

    docstring

--- openfe/utils/logging_filter.py
+++ openfe/utils/logging_filter.py
@@ -1,6 +1,16 @@
 import logging
 
 class MsgIncludesStringFilter:
+    """Logging filter to silence specfic log messages.
+
+    See https://docs.python.org/3/library/logging.html#filter-objects
+
+    Parameters
+    ----------
+    string : str
+        if an exact for this is included in the log message, the log record
+        is suppressed
+    """
     def __init__(self, string):
         self.string = string
 
