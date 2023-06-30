b81b195ba75f7332b57cb2d3ae9648bc40d50904
David W.H. Swenson <david.swenson@omsf.io>
2023-06-30 00:28:13 -0400

    a little more play

--- openfecli/commands/inspect_result.py
+++ openfecli/commands/inspect_result.py
@@ -5,15 +5,21 @@ def print_failure_unit_error(failure_unit):
     # let's make the output pretty
     print(tb_text)
 
+def unit_summary(unit_result):
+    ...
+
 def result_summary(result_dict, output):
     import math
-    summ_lines = []
     # we were success or failure
     success = "FAILURE" if math.isnan(result_dict['estimate']) else "SUCCESS"
-    summ_lines.append(f"This edge was a {success}")
+    yield f"This edge was a {success}."
     units = result_dict['unit_results']
-    summ_lines.append(f"This edge consists of {len(units)}")
-    for unit in ...
+    yield f"This edge consists of {len(units)} units."
+    for unit in units:
+        yield f"For unit ??unit label??"
+        unit_summ = unit_summary(unit)
+        ...
+
 
 
 def inspect_result(json_filename):
