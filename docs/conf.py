e307633dd9781d210d5f9cfabc3101e00a14da80
Josh Mitchell <yoshanuikabundi@gmail.com>
2023-07-12 20:11:02 +1000

    Add custom stylesheet for API reference

--- docs/conf.py
+++ docs/conf.py
@@ -12,16 +12,17 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
+
+sys.path.insert(0, os.path.abspath("../"))
 
 
-os.environ['SPHINX'] = 'True'
+os.environ["SPHINX"] = "True"
 
 # -- Project information -----------------------------------------------------
 
-project = 'OpenFE'
-copyright = '2022, The OpenFE Development Team'
-author = 'The OpenFE Development Team'
+project = "OpenFE"
+copyright = "2022, The OpenFE Development Team"
+author = "The OpenFE Development Team"
 
 # -- General configuration ---------------------------------------------------
 
@@ -29,16 +30,16 @@ author = 'The OpenFE Development Team'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx_click.ext',
-    'sphinxcontrib.autodoc_pydantic',
-    'sphinx_toolbox.collapse',
-    'sphinx.ext.autosectionlabel',
-    'sphinx_design',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_click.ext",
+    "sphinxcontrib.autodoc_pydantic",
+    "sphinx_toolbox.collapse",
+    "sphinx.ext.autosectionlabel",
+    "sphinx_design",
 ]
 
-autoclass_content = 'both'
+autoclass_content = "both"
 # Make sure labels are unique
 # https://www.sphinx-doc.org/en/master/usage/extensions/autosectionlabel.html#confval-autosectionlabel_prefix_document
 autosectionlabel_prefix_document = True
@@ -46,20 +47,21 @@ autosectionlabel_prefix_document = True
 autodoc_pydantic_model_show_json = False
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-autodoc_mock_imports = ['openff.models',
-                        'rdkit',
-                        'matplotlib',
-                        'lomap',
-                        'openmmtools',
-                        'mdtraj',
-                        'openmmforcefields',
+autodoc_mock_imports = [
+    "openff.models",
+    "rdkit",
+    "matplotlib",
+    "lomap",
+    "openmmtools",
+    "mdtraj",
+    "openmmforcefields",
 ]
 
 
@@ -68,11 +70,9 @@ autodoc_mock_imports = ['openff.models',
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'pydata_sphinx_theme'
+html_theme = "pydata_sphinx_theme"
 html_theme_options = {
-    "logo": {
-        "text": "OpenFE Documentation"
-    },
+    "logo": {"text": "OpenFE Documentation"},
     "icon_links": [
         {
             "name": "Github",
@@ -80,14 +80,14 @@ html_theme_options = {
             "icon": "fa-brands fa-square-github",
             "type": "fontawesome",
         }
-    ]
+    ],
 }
 html_logo = "_static/Squaredcircle.svg"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
+# html_static_path = ['_static']
 
 
 # replace macros
@@ -98,7 +98,8 @@ rst_prolog = """
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 html_css_files = [
-    'css/custom.css',
+    "css/custom.css",
+    "css/custom-api.css",
 ]
