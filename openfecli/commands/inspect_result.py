60fede2560a865be292e9b1f7be8836533f4c2d6
David W.H. Swenson <david.swenson@omsf.io>
2023-07-18 11:13:16 -0500

    Inspect result command

--- openfecli/commands/inspect_result.py
+++ openfecli/commands/inspect_result.py
@@ -1,26 +1,40 @@
+import json
+import warnings
 
-def print_failure_unit_error(failure_unit):
-    tb_text = failure_unit.traceback
-    # TODO: add a try/except around importing pygments; if it is there,
-    # let's make the output pretty
-    print(tb_text)
+def unit_summary(unit_label, unit):
+    qualname = unit['__qualname__']
+    if qualname == "ProtocolUnitResult":
+        yield f"Unit {unit_label} ran successfully."
+    elif qualname == "ProtocolUnitFailure":
+        yield f"Unit {unit_label} failed with an error:"
+        yield f"{unit['exception'][0]}: {unit['exception'][1][0]}"
+        yield f"{unit['traceback']}"
+    else:
+        warnings.warn(f"Unexpected result type '{aqualname}' from unit "
+                      f"{unit_label}")
 
-def unit_summary(unit_result):
-    ...
-
-def result_summary(result_dict, output):
+def result_summary(result_dict):
     import math
-    # we were success or failure
-    success = "FAILURE" if math.isnan(result_dict['estimate']) else "SUCCESS"
+    # we were success or failurea
+    estimate = result_dict['estimate']['magnitude']
+    success = "FAILURE" if math.isnan(estimate) else "SUCCESS"
     yield f"This edge was a {success}."
     units = result_dict['unit_results']
     yield f"This edge consists of {len(units)} units."
-    for unit in units:
-        yield f"For unit ??unit label??"
-        unit_summ = unit_summary(unit)
-        ...
-
+    yield ""
+    for unit_label, unit in units.items():
+        yield from unit_summary(unit_label, unit)
+        yield ""
 
 
 def inspect_result(json_filename):
-    ...
+    with open(json_filename, mode='r') as f:
+        result_dict = json.loads(f.read())
+
+    for line in result_summary(result_dict):
+        print(line)
+
+
+if __name__ == "__main__":
+    import sys
+    inspect_result(sys.argv[1])
