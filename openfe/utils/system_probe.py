9a46e022e64df02a7b32b7ade143bf0f61c214cc
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2023-07-31 15:13:13 -0700

    fetched more info, removed doc strings for now

--- openfe/utils/system_probe.py
+++ openfe/utils/system_probe.py
@@ -1,69 +1,76 @@
 import logging
+import os
 import socket
 import subprocess
 
+import psutil
+
+
+def _get_disk_usage() -> dict[str, dict[str, str]]:
+
+    output = subprocess.check_output(["df", "-h"]).decode("utf-8")
+
+    lines = output.strip().split(os.linesep)
+    lines = lines[1:]
+    disk_usage_dict = {}
+
+    for line in lines:
+        columns = line.split()
+
+        filesystem = columns[0]
+        size = columns[1]
+        used = columns[2]
+        available = columns[3]
+        percent_used = columns[4]
+        mount_point = columns[5]
+
+        disk_usage_dict[filesystem] = {
+            "size": size,
+            "used": used,
+            "available": available,
+            "percent_used": percent_used,
+            "mount_point": mount_point,
+        }
+
+    return disk_usage_dict
+
+
+def _get_psutil_info() -> dict[str, dict[str, str]]:
+
+    p = psutil.Process()
+
+    with p.oneshot():
+        info = p.as_dict(
+            attrs=[
+                "cpu_percent",
+                "create_time",
+                "exe",
+                "memory_full_info",
+                "memory_percent",
+                "num_fds",
+                "pid",
+                "status",
+            ]
+        )
+
+        # The maximum size of the process's virtual memory
+        RLIMIT_AS = p.rlimit(psutil.RLIMIT_AS)
+        mem = psutil.virtual_memory()
+
+    info["RLIMIT_AS"] = RLIMIT_AS
+    info["virtual_memory"] = mem
+
+    return info
+
 
 def _get_hostname() -> str:
-    """
-    Get the hostname of the current machine.
-
-    Returns
-    -------
-    str
-        The hostname of the current machine.
-
-    Notes
-    -----
-    This function uses the `socket` module to retrieve the standard hostname
-    for the current machine. The hostname represents the name of the machine
-    within a network environment.
-
-    Examples
-    --------
-    >>> _get_hostname()
-    'my-computer'
-    """
 
     return socket.gethostname()
 
 
 def _get_gpu_info() -> dict[str, dict[str, str]]:
-    """
-    Get GPU information using the `nvidia-smi` command.
 
-    Returns
-    -------
-    Dict[str, Dict[str, str]]
-        A dictionary containing GPU information. The keys of the outer dictionary
-        are GPU UUIDs, and the values are dictionaries containing GPU information.
-        The inner dictionaries have the following keys:
-            - 'gpu_name': str
-                The name of the GPU.
-            - 'compute_mode': str
-                The compute mode of the GPU.
-
-    Raises
-    ------
-    FileNotFoundError
-        If the `nvidia-smi` command is not found or NVIDIA drivers are not installed.
-        An empty dictionary will be returned.
-
-    Notes
-    -----
-    This function runs the `nvidia-smi` command to gather GPU information. It uses the `subprocess` module
-    to execute the command and parses the CSV output to build the GPU information dictionary.
-    If the `nvidia-smi` command is not found or no GPU is available, an empty dictionary is returned.
-
-    The GPU information includes the GPU UUID, name, and compute mode.
-
-    Examples
-    --------
-    >>> _get_gpu_info()
-    {'GPU-UUID-1': {'gpu_name': 'GeForce RTX 3080', 'compute_mode': 'Default'},
-     'GPU-UUID-2': {'gpu_name': 'GeForce GTX 1660', 'compute_mode': 'Exclusive Process'}}
-    """
-
-    GPU_QUERY = "--query-gpu=gpu_uuid,gpu_name,compute_mode"
+    GPU_QUERY = "--query-gpu=gpu_uuid,gpu_name,compute_mode,pstate,temperature.gpu,utilization.memory,memory.total,driver_version,"
 
     try:
         nvidia_smi_output = subprocess.check_output(
@@ -75,7 +82,7 @@ def _get_gpu_info() -> dict[str, dict[str, str]]:
         )
         return {}
 
-    nvidia_smi_output_lines = nvidia_smi_output.strip().split("\n")
+    nvidia_smi_output_lines = nvidia_smi_output.strip().split(os.linesep)
 
     header = nvidia_smi_output_lines[0].split(",")
 
@@ -97,45 +104,21 @@ def _get_gpu_info() -> dict[str, dict[str, str]]:
 
 
 def _probe_system() -> dict:
-    """
-    Probe the system to gather hostname and GPU information.
-
-    Returns
-    -------
-    dict
-        A dictionary containing system information. The keys of the outer dictionary are:
-            - 'system information': dict
-                A nested dictionary containing the following system information:
-                    - 'hostname': str
-                        The hostname of the current machine.
-                    - 'gpu information': dict
-                        A dictionary containing GPU information. The keys of this dictionary
-                        are GPU UUIDs, and the values are dictionaries containing GPU information.
-                        The inner dictionaries have the following keys:
-                            - 'gpu_name': str
-                                The name of the GPU.
-                            - 'compute_mode': str
-                                The compute mode of the GPU.
-
-    Notes
-    -----
-    This function calls the `_get_hostname()` and `_get_gpu_info()` functions to gather system information.
-    The `_get_hostname()` function retrieves the standard hostname for the current machine using the `socket` module.
-    The `_get_gpu_info()` function runs the `nvidia-smi` command (if available) to gather GPU information and returns a
-    dictionary containing GPU UUIDs, names, and compute modes.
-
-    If the `nvidia-smi` command is not found or no GPU is available, the 'gpu information' key in the returned dictionary
-    will be an empty dictionary.
-
-    Examples
-    --------
-    >>> _probe_system()
-    {'system information': {'hostname': 'my-computer',
-                            'gpu information': {'GPU-UUID-1': {'gpu_name': 'GeForce RTX 3080', 'compute_mode': 'Default'},
-                                                'GPU-UUID-2': {'gpu_name': 'GeForce GTX 1660', 'compute_mode': 'Exclusive Process'}}}}
-    """
 
     hostname = _get_hostname()
     gpu_info = _get_gpu_info()
+    psutil_info = _get_psutil_info()
+    disk_usage_info = _get_disk_usage()
 
-    return {"system information": {"hostname": hostname, "gpu information": gpu_info}}
+    return {
+        "system information": {
+            "hostname": hostname,
+            "gpu information": gpu_info,
+            "psutil information": psutil_info,
+            "disk usage information": disk_usage_info,
+        }
+    }
+
+
+if __name__ == "__main__":
+    print(_probe_system())
