4e4813cb86feeff9e152672fc43f23ce972b782b
David W.H. Swenson <david.swenson@omsf.io>
2023-08-01 14:30:54 -0500

    fix mypy complaints

--- openfe/setup/atom_mapping/lomap_scorers.py
+++ openfe/setup/atom_mapping/lomap_scorers.py
@@ -1,3 +1,5 @@
 # This code is part of OpenFE and is licensed under the MIT license.
 # For details, see https://github.com/OpenFreeEnergy/openfe
+from lomap.gufe_bindings.scorers import default_lomap_score  # hack for mypy
 from lomap.gufe_bindings.scorers import *
+
