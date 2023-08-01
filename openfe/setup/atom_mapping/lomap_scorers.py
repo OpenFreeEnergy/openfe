09182dab03db904ad506722671c513171a0cf321
David W.H. Swenson <david.swenson@omsf.io>
2023-08-01 15:07:35 -0500

    try exporting __all__ (make RTD happy too)

--- openfe/setup/atom_mapping/lomap_scorers.py
+++ openfe/setup/atom_mapping/lomap_scorers.py
@@ -1,5 +1,22 @@
 # This code is part of OpenFE and is licensed under the MIT license.
 # For details, see https://github.com/OpenFreeEnergy/openfe
-from lomap.gufe_bindings.scorers import default_lomap_score  # hack for mypy
+
+__all__ = [
+    'atomic_number_score',
+    'default_lomap_score',
+    'defaultdict',
+    'ecr_score',
+    'heterocycles_score',
+    'hybridization_score',
+    'lomap_mcs',
+    'math',
+    'mcsr_score',
+    'mncar_score',
+    'sulfonamides_score',
+    'tmcsr_score',
+    'transmuting_methyl_into_ring_score',
+    'transmuting_ring_sizes_score',
+]  # hack for numpy and RTD
+
 from lomap.gufe_bindings.scorers import *
 
