fe218cb57beccf8e60e5ccf877d99d9c0de0bb25
David W.H. Swenson <david.swenson@omsf.io>
2023-08-01 15:14:56 -0500

    guess mypy didn't like that at __all__

--- openfe/setup/atom_mapping/lomap_scorers.py
+++ openfe/setup/atom_mapping/lomap_scorers.py
@@ -1,22 +1,17 @@
 # This code is part of OpenFE and is licensed under the MIT license.
 # For details, see https://github.com/OpenFreeEnergy/openfe
 
-__all__ = [
-    'atomic_number_score',
-    'default_lomap_score',
-    'defaultdict',
-    'ecr_score',
-    'heterocycles_score',
-    'hybridization_score',
-    'lomap_mcs',
-    'math',
-    'mcsr_score',
-    'mncar_score',
-    'sulfonamides_score',
-    'tmcsr_score',
-    'transmuting_methyl_into_ring_score',
-    'transmuting_ring_sizes_score',
-]  # hack for numpy and RTD
-
-from lomap.gufe_bindings.scorers import *
+from lomap.gufe_bindings.scorers import (
+    atomic_number_score,
+    default_lomap_score,
+    ecr_score,
+    heterocycles_score,
+    hybridization_score,
+    mcsr_score,
+    mncar_score,
+    sulfonamides_score,
+    tmcsr_score,
+    transmuting_methyl_into_ring_score,
+    transmuting_ring_sizes_score,
+)  # looks like we gotta make it detailed for mypy and RTD
 
