f24e50619eb46bcbf3a33fddb6dfb3b0e397ebf8
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2023-08-29 14:16:31 -0700

    support pydantic v1 and v2

--- openfe/protocols/openmm_rfe/equil_rfe_settings.py
+++ openfe/protocols/openmm_rfe/equil_rfe_settings.py
@@ -9,7 +9,6 @@ energies using :class:`openfe.protocols.openmm_rfe.equil_rfe_methods.py`
 from __future__ import annotations
 
 from typing import Optional
-from pydantic import validator
 from openff.units import unit
 import os
 
@@ -23,6 +22,11 @@ from openfe.protocols.openmm_utils.omm_settings import (
     SystemSettings, SolvationSettings, AlchemicalSamplerSettings,
     OpenMMEngineSettings, IntegratorSettings, SimulationSettings
 )
+
+try:
+    from pydantic.v1 import validator
+except ImportError:
+    from pydantic import validator
 
 
 class AlchemicalSettings(SettingsBaseModel):
