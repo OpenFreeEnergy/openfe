21a0df6b154756c0461177e42cf70c7a34f9e8ff
richard gowers <richardjgowers@gmail.com>
2023-10-25 20:57:53 +0100

    adds yaml reader for cli options input
    fixes #580

---
+++ openfecli/commands/plan_network_schema.py
@@ -0,0 +1,62 @@
+# This code is part of OpenFE and is licensed under the MIT license.
+# For details, see https://github.com/OpenFreeEnergy/openfe
+"""Pydantic models for the definition of advanced CLI options
+
+"""
+
+from pydantic import BaseModel, ConfigDict
+from typing import Any, Optional
+import yaml
+import warnings
+
+
+class MapperSelection(BaseModel):
+    model_config = ConfigDict(extra='allow')
+
+    method: str = 'LomapAtomMapper'
+    settings: Optional[dict[str, Any]] = None
+
+
+class NetworkSelection(BaseModel):
+    model_config = ConfigDict(extra='allow')
+
+    method: str = 'generate_minimal_spanning_network'
+    settings: Optional[dict[str, Any]] = None
+
+
+class CliOptions(BaseModel):
+    model_config = ConfigDict(extra='allow')
+
+    mapper: Optional[MapperSelection] = None
+    network: Optional[NetworkSelection] = None
+
+
+def parse_yaml_planner_options(contents: str) -> CliOptions:
+    """Parse and minimally validate a user provided yaml
+
+    Parameters
+    ----------
+    contents : str
+      raw yaml formatted input to parse
+
+    Returns
+    -------
+    options : CliOptions
+      will have keys for mapper and network topology choices
+
+    Raises
+    ------
+    ValueError
+      for any malformed inputs
+    """
+    raw = yaml.safe_load(contents)
+
+    if False:
+        # todo: warnings about extra fields we don't expect?
+        expected = {'mapper', 'network'}
+        for field in raw:
+            if field in expected:
+                continue
+            warnings.warn(f"Unexpected attribute {field}")
+
+    return CliOptions(**raw)
---
+++ openfecli/tests/test_yaml_planner_options.py
@@ -0,0 +1,66 @@
+# This code is part of OpenFE and is licensed under the MIT license.
+# For details, see https://github.com/OpenFreeEnergy/openfe
+from openfecli.commands import plan_network_schema
+import pytest
+
+
+@pytest.fixture
+def full_yaml():
+    return """\
+mapper:
+  method: LomapAtomMapper
+  settings:
+    timeout: 120.0
+
+network:
+  method: generate_radial_network
+"""
+
+
+@pytest.fixture
+def partial_mapper_yaml():
+    return """\
+mapper:
+  method: KartografAtomMapper
+  settings:
+    timeout: 120.0
+"""
+
+@pytest.fixture
+def partial_network_yaml():
+    return """\
+network:
+  method: generate_radial_network
+  settings:
+    scorer: default_lomap_scorer
+"""
+
+
+def test_loading_full_yaml(full_yaml):
+    d = plan_network_schema.parse_yaml_planner_options(full_yaml)
+
+    assert d
+    assert d.mapper
+    assert d.mapper.method == 'LomapAtomMapper'
+    assert d.mapper.settings['timeout'] == 120
+    assert d.network
+    assert d.network.method == 'generate_radial_network'
+
+
+def test_loading_mapper_yaml(partial_mapper_yaml):
+    d = plan_network_schema.parse_yaml_planner_options(partial_mapper_yaml)
+
+    assert d
+    assert d.mapper
+    assert d.mapper.method == 'KartografAtomMapper'
+    assert d.network is None
+
+
+def test_loading_network_yaml(partial_network_yaml):
+    d = plan_network_schema.parse_yaml_planner_options(partial_network_yaml)
+
+    assert d
+    assert d.mapper is None
+    assert d.network
+    assert d.network.method == 'generate_radial_network'
+    assert d.network.settings['scorer'] == 'default_lomap_scorer'
