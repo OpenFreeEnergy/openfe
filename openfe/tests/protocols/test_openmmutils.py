2ac7bf12e8c58aa87f4d1836c6008fcfcf92fc2b
hannahbaumann <hbaumann@uci.edu>
2023-11-21 09:56:28 +0100

    Fix get_simsteps tests

--- openfe/tests/protocols/test_openmmutils.py
+++ openfe/tests/protocols/test_openmmutils.py
@@ -39,29 +39,25 @@ def test_get_simsteps(s, ts, mc, es):
 
 
 @pytest.mark.parametrize('nametype, timelengths', [
-    ['Equilibration', [1.003 * unit.picoseconds, 1 * unit.picoseconds]],
-    ['Production', [1 * unit.picoseconds, 1.003 * unit.picoseconds]],
+    ['Simulation', [1.003 * unit.picoseconds, 1 * unit.picoseconds]],
 ])
 def test_get_simsteps_indivisible_simtime(nametype, timelengths):
     errmsg = f"{nametype} time not divisible by timestep"
     with pytest.raises(ValueError, match=errmsg):
         settings_validation.get_simsteps(
                 timelengths[0],
-                timelengths[1],
                 2 * unit.femtoseconds,
                 100)
 
 
 @pytest.mark.parametrize('nametype, timelengths', [
-    ['Equilibration', [1 * unit.picoseconds, 10 * unit.picoseconds]],
-    ['Production', [10 * unit.picoseconds,  1 * unit.picoseconds]],
+    ['Simulation', [1 * unit.picoseconds, 10 * unit.picoseconds]],
 ])
 def test_mc_indivisible(nametype, timelengths):
     errmsg = f"{nametype} time 1.0 ps should contain"
     with pytest.raises(ValueError, match=errmsg):
         settings_validation.get_simsteps(
-                timelengths[0], timelengths[1],
-                2 * unit.femtoseconds, 1000)
+                timelengths[0], 2 * unit.femtoseconds, 1000)
 
 
 def test_get_alchemical_components(benzene_modifications,
