7e44518ed9a7cb811e1e85663f8e6de5a769af32
hannahbaumann <hbaumann@uci.edu>
2024-01-30 12:05:47 +0100

    Change keys

--- openfe/tests/protocols/test_solvation_afe_tokenization.py
+++ openfe/tests/protocols/test_solvation_afe_tokenization.py
@@ -49,7 +49,7 @@ def protocol_result(afe_solv_transformation_json):
 
 class TestAbsoluteSolvationProtocol(GufeTokenizableTestsMixin):
     cls = openmm_afe.AbsoluteSolvationProtocol
-    key = "AbsoluteSolvationProtocol-fd22076bcea777207beb86ef7a6ded81"
+    key = "AbsoluteSolvationProtocol-a7509742e4142e75ea7a77439e11c6a2"
     repr = f"<{key}>"
 
     @pytest.fixture()
@@ -85,7 +85,7 @@ class TestAbsoluteSolvationVacuumUnit(GufeTokenizableTestsMixin):
 
 class TestAbsoluteSolvationProtocolResult(GufeTokenizableTestsMixin):
     cls = openmm_afe.AbsoluteSolvationProtocolResult
-    key = "AbsoluteSolvationProtocolResult-e7d74b8ccc009d071b8c6eb0420da4bf"
+    key = "AbsoluteSolvationProtocolResult-e9590f4df17ff340afe5d734f602773a"
     repr = f"<{key}>"
 
     @pytest.fixture()
