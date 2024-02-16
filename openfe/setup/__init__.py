5ef8b8fb9a385677e69d30f3f16a50b0a21742b5
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2024-02-16 00:49:02 -0700

    swapping the order of the imports in openfe/setup/__init__.py breaks things

--- openfe/setup/__init__.py
+++ openfe/setup/__init__.py
@@ -1,18 +1,18 @@
 # This code is part of OpenFE and is licensed under the MIT license.
 # For details, see https://github.com/OpenFreeEnergy/openfe
-
+# isort: skip_file
 
 from .atom_mapping import (
-    LigandAtomMapping,
-    LigandAtomMapper,
-    LomapAtomMapper,
-    lomap_scorers,
-    PersesAtomMapper,
-    perses_scorers,
     KartografAtomMapper,
+    LigandAtomMapper,
+    LigandAtomMapping,
+    LomapAtomMapper,
+    PersesAtomMapper,
+    lomap_scorers,
+    perses_scorers,
 )
 
 from gufe import LigandNetwork
-from . import ligand_network_planning
 
-from .alchemical_network_planner import RHFEAlchemicalNetworkPlanner, RBFEAlchemicalNetworkPlanner
+from . import ligand_network_planning
+from .alchemical_network_planner import RBFEAlchemicalNetworkPlanner, RHFEAlchemicalNetworkPlanner
