9911207a26bda1abcb0e278c89de34c4032ad9f4
Irfan Alibay <IAlibay@users.noreply.github.com>
2024-07-04 00:16:17 +0100

    Update test_solvation_afe_tokenization.py

--- openfe/tests/protocols/test_solvation_afe_tokenization.py
+++ openfe/tests/protocols/test_solvation_afe_tokenization.py
@@ -49,7 +49,7 @@ def protocol_result(afe_solv_transformation_json):
 
 class TestAbsoluteSolvationProtocol(GufeTokenizableTestsMixin):
     cls = openmm_afe.AbsoluteSolvationProtocol
-    key = "AbsoluteSolvationProtocol-c602c05772bd839d7717f4820d2961e1"
+    key = "AbsoluteSolvationProtocol-36e2e292503864aac09e2d5066f24be1"
     repr = f"<{key}>"
 
     @pytest.fixture()
