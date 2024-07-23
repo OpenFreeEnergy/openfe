903af15fd3acd6703a948f340c14edfb2df4a4c2
IAlibay <ialibay@mdanalysis.org>
2024-07-22 21:02:08 -0400

    Ignore extra flatten calls

--- openfe/analysis/plotting.py
+++ openfe/analysis/plotting.py
@@ -303,8 +303,8 @@ def plot_2D_rmsd(data: list[list[float]],
 
     # if we have any leftover plots then we turn them off
     # except the last one!
-    overage = len(axes.flatten()) - len(twod_rmsd_arrs)
-    for i in range(overage, len(axes.flatten())-1):
+    overage = len(axes.flatten()) - len(twod_rmsd_arrs)  # type: ignore
+    for i in range(overage, len(axes.flatten())-1):  # type: ignore
         axes.flatten()[i].set_axis_off()
 
     plt.colorbar(axes.flatten()[0].images[0],  # type: ignore
