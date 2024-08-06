788c002a14a022b1ffe426b8f81f382438125e3d
Matthew W. Thompson <mattwthompson@protonmail.com>
2024-08-06 11:29:10 -0500

    Use `from pydantic.v1` everywhere

--- openfe/protocols/openmm_utils/omm_settings.py
+++ openfe/protocols/openmm_utils/omm_settings.py
@@ -21,10 +21,7 @@ from gufe.settings import (
 )
 
 
-try:
-    from pydantic.v1 import validator
-except ImportError:
-    from pydantic import validator  # type: ignore[assignment]
+from pydantic.v1 import validator
 
 
 class BaseSolvationSettings(SettingsBaseModel):
