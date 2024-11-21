32a5590870009880febd709eb6c6bc2a7a82659a
Alyssa Travitz <atravitz@umich.edu>
2024-11-21 08:53:17 -0800

    adding TODO

--- openfecli/commands/quickrun.py
+++ openfecli/commands/quickrun.py
@@ -102,9 +102,9 @@ def quickrun(transformation, work_dir, output):
         output = work_dir / (str(trans.key) + '_results.json')
 
     try:
+        # make sure the output file is writeable before running simulations
         with open(output, mode='w') as outf:
-            # json.dump(out_dict, outf, cls=JSON_HANDLER.encoder)
-            outf.write("in-progress")
+            outf.write("in-progress")  # TODO: do we just want to pass here?
     except FileNotFoundError as e:
         raise click.ClickException(f"Unable to write to {output}, please provide a valid path.")
 
