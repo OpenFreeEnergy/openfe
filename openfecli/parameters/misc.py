73e84b5133c1255817f78c500974573ebde9b7ff
Alyssa Travitz <alyssa.travitz@omsf.io>
2025-01-17 15:32:33 -0800

    clarifying language

--- openfecli/parameters/misc.py
+++ openfecli/parameters/misc.py
@@ -5,5 +5,7 @@ N_PROTOCOL_REPEATS = Option(
     "-n",
     "--n-protocol-repeats",
     type=click.INT,
-    help="The number of completely independent repeats of the entire sampling process.",
+    help="The number of completely independent repeats per protocol unit.  "\
+        "For example, setting ``--n-protocol-repeats=1`` can allow for each individual repeat to be submitted in parallel,"\
+        "whereas ``--n-protocol-repeats=3`` would run 3 repeats in serial within a single unit.",
 )
