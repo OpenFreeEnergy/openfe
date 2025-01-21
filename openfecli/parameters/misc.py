ea96d28b5e28b1b283e9a3b435c9c55c6535a28f
Alyssa Travitz <alyssa.travitz@omsf.io>
2025-01-21 09:29:10 -0800

    clarifying language

--- openfecli/parameters/misc.py
+++ openfecli/parameters/misc.py
@@ -4,7 +4,7 @@ from plugcli.params import Option
 N_PROTOCOL_REPEATS = Option(
     "--n-protocol-repeats",
     type=click.INT,
-    help="The number of completely independent repeats per protocol unit.\n"
+    help="Number of independent repeats per protocol unit.\n"
     "For example, setting ``--n-protocol-repeats=1`` can allow for each individual repeat to be submitted in parallel, "
-    "whereas ``--n-protocol-repeats=3`` would run 3 repeats in serial within a single unit.",
+    "whereas ``--n-protocol-repeats=3`` would run 3 repeats in serial within a single protocol unit.",
 )
