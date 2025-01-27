658f69206328e0e31b2566966fd5870fff936606
Alyssa Travitz <alyssa.travitz@omsf.io>
2025-01-27 13:57:00 -0800

    adding no-cov for test utils

--- openfecli/tests/utils.py
+++ openfecli/tests/utils.py
@@ -4,7 +4,7 @@ import click
 import traceback
 
 
-def assert_click_success(result: click.testing.Result):
+def assert_click_success(result: click.testing.Result): # -no-cov-
     """Pass through error message if a click test fails.
     Taken from https://github.com/openpathsampling/openpathsampling-cli/blob/main/paths_cli/commands/pathsampling.py
     """
