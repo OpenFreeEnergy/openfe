848f350b7f076e6b3fa61b04ff80117d24805178
Alyssa Travitz <31974495+atravitz@users.noreply.github.com>
2025-01-29 10:57:37 -0800

    Update openfecli/parameters/misc.py
    Co-authored-by: Irfan Alibay <IAlibay@users.noreply.github.com>

--- openfecli/parameters/misc.py
+++ openfecli/parameters/misc.py
@@ -4,7 +4,7 @@ from plugcli.params import Option
 N_PROTOCOL_REPEATS = Option(
     "--n-protocol-repeats",
     type=click.INT,
-    help="Number of independent run(s) to be run per execution of a transformation using the `openfe quickrun` command.\n\n"
+    help="Number of independent repeats(s) to be run per execution of a transformation using the `openfe quickrun` command.\n\n"
     "For example:\n\n  `--n-protocol-repeats=3` means `openfe quickrun` will execute 3 repeats in serial.\n\n"
     "  `--n-protocol-repeats=1` means `openfe quickrun` will execute only 1 repeat per call, "
     "which allows for individual repeats to be submitted in parallel by calling `openfe quickrun` on the same input JSON file multiple times.",
