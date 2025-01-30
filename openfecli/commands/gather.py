350174d5442afbe77e56c9252394f4044b8b7b17
Alyssa Travitz <alyssa.travitz@omsf.io>
2025-01-30 15:49:07 -0800

    adding print statements, tests don't pass yet because of extra output

--- openfecli/commands/gather.py
+++ openfecli/commands/gather.py
@@ -103,13 +103,16 @@ def load_and_check_result(fpath:os.PathLike|str)->dict:
         return None
 
     if "unit_results" not in result.keys():
-        click.echo(f"{fpath} has no unit results - likely a failure.", err=True)
+        click.echo(f"{fpath}: No 'unit_results' found, assuming to be a failed simulation.", err=True)
 
     if all('exception' in u for u in result['unit_results'].values()):
-        click.echo(f"{fpath} is a failed simulation.", err=True)
+        click.echo(f"{fpath}: Exception found in 'unit_results', assuming to be a failed simulation.", err=True)
 
-    if result['estimate'] is None or result['uncertainty'] is None:
-        click.echo(f"{fpath} is a failed simulation", err=True)
+    if result['estimate'] is None:
+        click.echo(f"No{fpath}: No 'estimate' found, assuming to be a failed simulation", err=True)
+
+    if result['uncertainty'] is None:
+        click.echo(f"{fpath}: No 'uncertainty' found, assuming to be a failed simulation", err=True)
 
     return result
 
