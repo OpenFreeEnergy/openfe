7eff55fa3d5bbe67c50e4bf8e5c4ade33e8c8c86
Alyssa Travitz <alyssa.travitz@omsf.io>
2025-04-22 07:45:18 -0700

    update syntax for numpy 2 support

--- openfe/protocols/openmm_rfe/equil_rfe_methods.py
+++ openfe/protocols/openmm_rfe/equil_rfe_methods.py
@@ -953,9 +953,9 @@ class RelativeHybridTopologyProtocolUnit(gufe.ProtocolUnit):
 
         #  b. Write out a PDB containing the subsampled hybrid state
         bfactors = np.zeros_like(selection_indices, dtype=float)  # solvent
-        bfactors[np.in1d(selection_indices, list(hybrid_factory._atom_classes['unique_old_atoms']))] = 0.25  # lig A
-        bfactors[np.in1d(selection_indices, list(hybrid_factory._atom_classes['core_atoms']))] = 0.50  # core
-        bfactors[np.in1d(selection_indices, list(hybrid_factory._atom_classes['unique_new_atoms']))] = 0.75  # lig B
+        bfactors[np.isin(selection_indices, list(hybrid_factory._atom_classes['unique_old_atoms']))] = 0.25  # lig A
+        bfactors[np.isin(selection_indices, list(hybrid_factory._atom_classes['core_atoms']))] = 0.50  # core
+        bfactors[np.isin(selection_indices, list(hybrid_factory._atom_classes['unique_new_atoms']))] = 0.75  # lig B
         # bfactors[np.in1d(selection_indices, protein)] = 1.0  # prot+cofactor
 
         if len(selection_indices) > 0:
