edf82800ee1faeb559bc9a8d47f2ef2713ed68f2
IAlibay <IAlibay@users.noreply.github.com>
2025-05-24 22:17:15 +0100

    Add complex sim long test

--- openfe/tests/protocols/openmm/plain_md/test_simulation_slow.py
+++ openfe/tests/protocols/openmm/plain_md/test_simulation_slow.py
@@ -75,3 +75,70 @@ def test_vacuum_sim(
     assert pur.outputs['last_checkpoint'] == unit_shared / "checkpoint.chk"
     assert pur.outputs['npt_equil_pdb'] == unit_shared / "equil_npt.pdb"
     assert "nvt_equil_pdb" not in pur.outputs.keys()
+
+
+@pytest.mark.integration
+@pytest.mark.parametrize('platform', ['CUDA'])
+def test_complex_solvent_sim_gpu(
+    benzene_complex_system,
+    platform,
+    available_platforms,
+    tmpdir,
+):
+    if platform not in available_platforms:
+        pytest.skip(f"OpenMM Platform: {platform} is not available")
+
+    # Run an MD simulation and check what files we get.
+    settings = openmm_md.PlainMDProtocol.default_settings()
+    settings.simulation_settings.equilibration_length_nvt = 50 * unit.picosecond
+    settings.simulation_settings.equilibration_length = 50 * unit.picosecond
+    settings.simulation_settings.production_length = 100 * unit.picosecond
+    settings.output_settings.checkpoint_interval = 10 * unit.picosecond
+    settings.engine_settings.compute_platform = platform
+
+    prot = openmm_md.PlainMDProtocol(settings)
+
+    dag = prot.create(
+        stateA=benzene_complex_system,
+        stateB=benzene_complex_system,
+        mapping=None,
+    )
+
+    workdir = pathlib.Path(str(tmpdir))
+
+    r = execute_DAG(
+        dag,
+        shared_basedir=workdir,
+        scratch_basedir=workdir,
+        keep_shared=True
+    )
+
+    assert r.ok()
+
+    assert len(r.protocol_unit_results) == 1
+
+    pur = r.protocol_unit_results[0]
+    unit_shared = tmpdir / f"shared_{pur.source_key}_attempt_0"
+    assert unit_shared.exists()
+    assert pathlib.Path(unit_shared).is_dir()
+
+    # check the files
+    files = [
+        "checkpoint.chk",
+        "equil_nvt.pdb",
+        "equil_npt.pdb",
+        "minimized.pdb",
+        "simulation.xtc",
+        "simulation.log",
+        "system.pdb"
+    ]
+    for file in files:
+        assert (unit_shared / file).exists()
+
+    # check that the output file paths are correct
+    assert pur.outputs['system_pdb'] == unit_shared / "system.pdb"
+    assert pur.outputs['minimized_pdb'] == unit_shared / "minimized.pdb"
+    assert pur.outputs['trajectory'] == unit_shared / "simulation.xtc"
+    assert pur.outputs['last_checkpoint'] == unit_shared / "checkpoint.chk"
+    assert pur.outputs['nvt_equil_pdb'] == unit_shared / "equil_nvt.pdb"
+    assert pur.outputs['npt_equil_pdb'] == unit_shared / "equil_npt.pdb"
