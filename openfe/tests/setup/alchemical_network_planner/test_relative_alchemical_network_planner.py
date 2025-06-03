385dbf171e0b293c862aa01a6f18128dc0108952
Alyssa Travitz <alyssa.travitz@omsf.io>
2025-06-03 13:47:47 -0700

    updating TODO

--- openfe/tests/setup/alchemical_network_planner/test_relative_alchemical_network_planner.py
+++ openfe/tests/setup/alchemical_network_planner/test_relative_alchemical_network_planner.py
@@ -33,7 +33,9 @@ def test_rbfe_alchemical_network_planner_call(atom_mapping_basic_test_files, T4_
         solvent=SolventComponent(),
         protein=T4_protein_component,
     )
-    # TODO: methylcyclohexane isn't included in this network, possibly because of a score tie in _map_scoring, which means it doesn't get included in maximal_network_generator's "mappings"
+    # TODO:this network is slightly different when using konnektor,
+    # possibly because of a score tie in _map_scoring,
+    # which means it doesn't get included in maximal_network_generator's "mappings"
     assert isinstance(alchem_network, AlchemicalNetwork)
 
     edges = alchem_network.edges
