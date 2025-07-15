d849b091f6003140bdc83180fd86de3251457cec
Alyssa Travitz <alyssa.travitz@omsf.io>
2025-07-15 08:37:11 -0700

    try using as_file wrapper

--- openfe/tests/conftest.py
+++ openfe/tests/conftest.py
@@ -1,7 +1,6 @@
 # This code is part of OpenFE and is licensed under the MIT license.
 # For details, see https://github.com/OpenFreeEnergy/openfe
 import os
-import importlib
 import pytest
 from importlib import resources
 from rdkit import Chem
@@ -150,10 +149,10 @@ def lomap_basic_test_files_dir(tmpdir_factory):
     lomap_files = tmpdir_factory.mktemp('lomap_files')
     lomap_basic = 'openfe.tests.data.lomap_basic'
 
-    for f in importlib.resources.contents(lomap_basic):
+    for f in resources.contents(lomap_basic):
         if not f.endswith('mol2'):
             continue
-        stuff = importlib.resources.read_binary(lomap_basic, f)
+        stuff = resources.read_binary(lomap_basic, f)
 
         with open(str(lomap_files.join(f)), 'wb') as fout:
             fout.write(stuff)
@@ -175,7 +174,7 @@ def atom_mapping_basic_test_files():
         '2-naftanol',
         'methylcyclohexane',
         'toluene']:
-        with importlib.resources.files('openfe.tests.data.lomap_basic') as d:
+        with resources.as_file(resources.files('openfe.tests.data.lomap_basic')) as d:
             fn = str(d / (f + '.mol2'))
             mol = Chem.MolFromMol2File(fn, removeHs=False)
             files[f] = SmallMoleculeComponent(mol, name=f)
@@ -204,7 +203,7 @@ def lomap_old_mapper() -> AtomMapper:
 @pytest.fixture(scope='session')
 def benzene_modifications():
     files = {}
-    with importlib.resources.files('openfe.tests.data') as d:
+    with resources.as_file(resources.files('openfe.tests.data')) as d:
         fn = str(d / 'benzene_modifications.sdf')
         supp = Chem.SDMolSupplier(str(fn), removeHs=False)
         for rdmol in supp:
@@ -216,7 +215,7 @@ def benzene_modifications():
 def serialization_template():
     def inner(filename):
         loc = "openfe.tests.data.serialization"
-        tmpl = importlib.resources.read_text(loc, filename)
+        tmpl = resources.read_text(loc, filename)
         return tmpl.replace('{OFE_VERSION}', openfe.__version__)
 
     return inner
@@ -236,7 +235,7 @@ def benzene_transforms():
 
 @pytest.fixture(scope='session')
 def T4_protein_component():
-    with resources.files('openfe.tests.data') as d:
+    with resources.as_file(resources.files('openfe.tests.data')) as d:
         fn = str(d / '181l_only.pdb')
         comp = gufe.ProteinComponent.from_pdb_file(fn, name="T4_protein")
 
@@ -294,7 +293,7 @@ def CN_molecule():
     """
     A basic CH3NH2 molecule for quick testing.
     """
-    with resources.files('openfe.tests.data') as d:
+    with resources.as_file(resources.files('openfe.tests.data')) as d:
         fn = str(d / 'CN.sdf')
         supp = Chem.SDMolSupplier(str(fn), removeHs=False)
 
