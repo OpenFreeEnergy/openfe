ef605e6bed712b07815a0eb8b3cfb2c4dfb074e5
Hannah Baumann <43765638+hannahbaumann@users.noreply.github.com>
2025-09-25 12:02:54 +0200

    Update openfe/protocols/openmm_septop/equil_septop_method.py
    Co-authored-by: Irfan Alibay <IAlibay@users.noreply.github.com>

--- openfe/protocols/openmm_septop/equil_septop_method.py
+++ openfe/protocols/openmm_septop/equil_septop_method.py
@@ -1795,7 +1795,15 @@ class SepTopComplexSetupUnit(SepTopComplexMixin, BaseSepTopSetupUnit):
             )
         )
 
-        smc_off_B = {m: m.to_openff() for m in alchem_comps["stateB"]}
+        smc_B_unique_keys = smc_comps_B.keys() - smc_comp_A.keys()
+        smc_comp_B_unique = {key: smc_comps_B[key] for key in smc_B_unique_keys}
+        omm_system_AB, omm_topology_AB, positions_AB, modeller_AB = self.get_system_AB(
+            solv_comp,
+            modeller_A,
+            smc_comps_AB,
+            smc_comp_B_unique,
+            settings,
+        )
         omm_system_AB, omm_topology_AB, positions_AB, modeller_AB = self.get_system_AB(
             solv_comp,
             modeller_A,
