53b055f8221b3a52ba765befaf2958a63ded1c87
Alyssa Travitz <alyssa.travitz@omsf.io>
2025-10-10 08:21:36 -0700

    add units to box vector test

--- openfe/tests/protocols/test_openmm_settings.py
+++ openfe/tests/protocols/test_openmm_settings.py
@@ -77,7 +77,7 @@ class TestOpenMMSolvationSettings:
                 [0.0, 1.0, 0.0],
                 [1.0, 0.0, 0.0],
             ],
-        )
+        ) * unit.nanometer
 
         with pytest.raises(ValueError, match="not in OpenMM reduced form"):
             s.box_vectors = box_vectors
