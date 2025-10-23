5bfd1c9d1a7f9cc39d4f3e0a39462adaa3f040ea
hannahbaumann <hbaumann@uci.edu>
2025-10-23 09:29:05 +0200

    Prints for debugging

--- openfe/protocols/openmm_rfe/equil_rfe_methods.py
+++ openfe/protocols/openmm_rfe/equil_rfe_methods.py
@@ -951,6 +951,7 @@ class RelativeHybridTopologyProtocolUnit(gufe.ProtocolUnit):
             softcore_LJ_v2_alpha=alchem_settings.softcore_alpha,
             interpolate_old_and_new_14s=alchem_settings.turn_off_core_unique_exceptions,
         )
+        print(hybrid_factory.hybrid_system.getForces())
 
         # 4. Create lambda schedule
         # TODO - this should be exposed to users, maybe we should offer the
