cbcfa636094c8555c11a9cd6b66e7f029df1c9ac
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2025-10-28 16:52:34 -0700

    add missing import

--- openfe/vendor/perses/atom_mapping.py
+++ openfe/vendor/perses/atom_mapping.py
@@ -3,6 +3,9 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+#TODO: Follow OpenFE convention
+from simtk import unit
+
 class InvalidMappingException(Exception):
     """
     Invalid atom mapping for relative free energy transformation.
