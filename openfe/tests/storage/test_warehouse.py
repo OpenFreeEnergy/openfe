2fb82e1f886e42cecbafda69e8f92597b1b463fd
Ethan Holz <ethan.holz@omsf.io>
2025-11-12 16:45:07 -0700

    test: add a test for the FileSystemWarehouse

--- openfe/tests/storage/test_warehouse.py
+++ openfe/tests/storage/test_warehouse.py
@@ -1,4 +1,6 @@
 import os
+import tempfile
+from pathlib import Path
 
 import pytest
 from unittest import mock
@@ -6,6 +8,7 @@ from unittest import mock
 from gufe.storage.externalresource import MemoryStorage
 from gufe.tokenization import GufeTokenizable
 from openfe.storage.warehouse import (
+    FileSystemWarehouse,
     WarehouseBaseClass,
     WarehouseStores,
 )
@@ -105,3 +108,59 @@ class TestWarehouseBaseClass:
         assert client.setup_store.exists(key)
         client.delete("setup", key)
         assert not client.exists(key)
+
+
+class TestFileSystemWarehouse:
+    @staticmethod
+    def _test_store_load_same_process(obj, store_func_name, load_func_name):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            client = FileSystemWarehouse(tmpdir)
+            store_func = getattr(client, store_func_name)
+            load_func = getattr(client, load_func_name)
+            assert not any(Path(f"{tmpdir}").iterdir())
+            store_func(obj)
+            assert any(Path(f"{tmpdir}").iterdir())
+            reloaded = load_func(obj.key)
+            assert reloaded is obj
+
+    @staticmethod
+    def _test_store_load_different_process(obj: GufeTokenizable, store_func_name, load_func_name):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            client = FileSystemWarehouse(tmpdir)
+            store_func = getattr(client, store_func_name)
+            load_func = getattr(client, load_func_name)
+            assert not any(Path(f"{tmpdir}").iterdir())
+            store_func(obj)
+            assert any(Path(f"{tmpdir}").iterdir())
+            # make it look like we have an empty cache, as if this was a
+            # different process
+            key = obj.key
+            registry_dict = "gufe.tokenization.TOKENIZABLE_REGISTRY"
+            with mock.patch.dict(registry_dict, {}, clear=True):
+                reload = load_func(key)
+                assert reload == obj
+                assert reload is not obj
+
+    @pytest.mark.parametrize(
+        "fixture",
+        ["absolute_transformation", "complex_equilibrium"],
+    )
+    def test_store_load_transformation_same_process(self, request, fixture):
+        transformation = request.getfixturevalue(fixture)
+        self._test_store_load_same_process(
+            transformation,
+            "store_setup_tokenizable",
+            "load_setup_tokenizable",
+        )
+
+    @pytest.mark.parametrize(
+        "fixture",
+        ["absolute_transformation", "complex_equilibrium"],
+    )
+    def test_store_load_transformation_different_process(self, request, fixture):
+        transformation = request.getfixturevalue(fixture)
+        self._test_store_load_different_process(
+            transformation,
+            "store_setup_tokenizable",
+            "load_setup_tokenizable",
+        )
