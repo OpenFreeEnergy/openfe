e13fc2e0c70bb4280c2aa2b48b4af15ee1ca60d4
Alyssa Travitz <alyssa.travitz@omsf.io>
2025-11-18 14:54:03 -0800

    add abfe MVP with tests

---
+++ openfecli/commands/gather_abfe.py
@@ -0,0 +1,315 @@
+import os
+import pathlib
+from typing import List, Literal
+
+import click
+import numpy as np
+import pandas as pd
+from cinnabar import FEMap, Measurement
+from openff.units import unit
+
+from openfecli import OFECommandPlugin
+from openfecli.clicktypes import HyphenAwareChoice
+from openfecli.commands.gather import (
+    _collect_result_jsons,
+    format_estimate_uncertainty,
+    load_json,
+    rich_print_to_stdout,
+)
+
+
+def _get_name(result: dict) -> str:
+    """Get the ligand name from a unit's results data.
+
+    Parameters
+    ----------
+    result : dict
+        A results dict.
+
+    Returns
+    -------
+    str
+        Ligand name corresponding to the results.
+    """
+    try:
+        nm = list(result["unit_results"].values())[0]["name"]
+
+    except KeyError:
+        raise ValueError("Failed to guess name")
+
+    toks = nm.split("Binding, ")
+    if "solvent" in toks[1]:
+        name = toks[1].split(" solvent")[0]
+    if "complex" in toks[1]:
+        name = toks[1].split(" complex")[0]
+    return name
+
+
+def _load_valid_result_json(fpath: os.PathLike | str) -> tuple[tuple | None, dict | None]:
+    """Load the data from a results JSON into a dict.
+
+    Parameters
+    ----------
+    fpath : os.PathLike | str
+        The path to deserialized results.
+
+    Returns
+    -------
+    dict | None
+        A dict containing data from the results JSON,
+        or None if the JSON file is invalid or missing.
+
+    Raises
+    ------
+    ValueError
+      If the JSON file contains an ``estimate`` or ``uncertainty`` key with the
+      value ``None``.
+      If
+    """
+    # TODO: replace this with gather's _load_valid_result
+
+    # TODO: only load this once during collection, then pass namedtuple(fname, dict) into this function
+    # for now though, it's not the bottleneck on performance
+    result = load_json(fpath)
+    try:
+        names = _get_name(result)
+    except (ValueError, IndexError):
+        click.secho(f"{fpath}: Missing ligand names and/or simulation type. Skipping.",err=True, fg="yellow")  # fmt: skip
+        return None, None
+    if result["estimate"] is None:
+        click.secho(f"{fpath}: No 'estimate' found, assuming to be a failed simulation.",err=True, fg="yellow")  # fmt: skip
+        return names, None
+    if result["uncertainty"] is None:
+        click.secho(f"{fpath}: No 'uncertainty' found, assuming to be a failed simulation.",err=True, fg="yellow")  # fmt: skip
+        return names, None
+    if all("exception" in u for u in result["unit_results"].values()):
+        click.secho(f"{fpath}: Exception found in all 'unit_results', assuming to be a failed simulation.",err=True, fg="yellow")  # fmt: skip
+        return names, None
+    return names, result
+
+
+def _get_legs_from_result_jsons(
+    result_fns: list[pathlib.Path],
+) -> dict[str, dict[str, list]]:
+    """
+    Iterate over a list of result JSONs and populate a dict of dicts with all data needed
+    for results processing.
+
+
+    Parameters
+    ----------
+    result_fns : list[pathlib.Path]
+        List of filepaths containing results formatted as JSON.
+    report : Literal["dg", "ddg", "raw"]
+        Type of report to generate.
+
+    Returns
+    -------
+    legs: dict[str, dict[str, list]]
+        Data extracted from the given result JSONs, organized by the leg's ligand names and simulation type.
+    """
+    from collections import defaultdict
+
+    ddgs = defaultdict(lambda: defaultdict(list))
+
+    for result_fn in result_fns:
+        names, result = _load_valid_result_json(result_fn)
+        if names is None:  # this means it couldn't find names and/or simtype
+            continue
+
+        ddgs[names]["overall"].append([result["estimate"], result["uncertainty"]])
+        proto_key = [
+            k
+            for k in result["unit_results"].keys()
+            if k.startswith("ProtocolUnitResult")
+        ]  # fmt: skip
+        for p in proto_key:
+            if "unit_estimate" in result["unit_results"][p]["outputs"]:
+                simtype = result["unit_results"][p]["outputs"]["simtype"]
+                dg = result["unit_results"][p]["outputs"]["unit_estimate"]
+                dg_error = result["unit_results"][p]["outputs"]["unit_estimate_error"]
+
+                ddgs[names][simtype].append([dg, dg_error])
+            elif "standard_state_correction_A" in result["unit_results"][p]["outputs"]:
+                corr_A = result["unit_results"][p]["outputs"]["standard_state_correction_A"]
+                corr_B = result["unit_results"][p]["outputs"]["standard_state_correction_B"]
+                ddgs[names]["standard_state_correction_A"].append(
+                    [corr_A, 0 * unit.kilocalorie_per_mole]
+                )
+                ddgs[names]["standard_state_correction_B"].append(
+                    [corr_B, 0 * unit.kilocalorie_per_mole]
+                )
+            else:
+                continue
+
+    return ddgs
+
+
+def _error_std(r):
+    """
+    Calculate the error of the estimate as the std of the repeats
+    """
+    return np.std([v[0].m for v in r["overall"]])
+
+
+def _error_mbar(r):
+    """
+    Calculate the error of the estimate using the reported MBAR errors.
+
+    This also takes into account that repeats may have been run for this edge by using the average MBAR error
+    """
+    complex_errors = [x[1].m for x in r["complex"]]
+    solvent_errors = [x[1].m for x in r["solvent"]]
+    return np.sqrt(np.mean(complex_errors) ** 2 + np.mean(solvent_errors) ** 2)
+
+
+def extract_results_dict(
+    results_files: list[os.PathLike | str],
+) -> dict[str, dict[str, list]]:
+    """
+    Get a dictionary of abfe results from a list of directories.
+
+    Parameters
+    ----------
+    results_files : list[ps.PathLike | str]
+        A list of directors with abfe result files to process.
+
+    Returns
+    -------
+    sim_results : dict[str, dict[str, list]]
+        Simulation results, organized by the leg's ligand names and simulation type.
+    """
+    # find and filter result jsons
+    result_fns = _collect_result_jsons(results_files)
+    # pair legs of simulations together into dict of dicts
+    sim_results = _get_legs_from_result_jsons(result_fns)
+
+    return sim_results
+
+
+def generate_dg(results_dict: dict[str, dict[str, list]]) -> pd.DataFrame:
+    """Compute and write out DG values for the given results.
+
+    Parameters
+    ----------
+    results_dict : dict[str, dict[str, list]]
+        Dictionary of results created by ``extract_results_dict``.
+
+    Returns
+    -------
+    pd.DataFrame
+        A pandas DataFrame with the dG results for each ligand.
+    """
+    data = []
+    # check the type of error which should be used based on the number of repeats
+    repeats = {len(v["overall"]) for v in results_dict.values()}
+    error_func = _error_mbar if 1 in repeats else _error_std
+    for lig, results in sorted(results_dict.items()):
+        dg = np.mean([v[0].m for v in results["overall"]])
+        error = error_func(results)
+        m, u = format_estimate_uncertainty(dg, error, unc_prec=2)
+        data.append((lig, m, u))
+
+    df = pd.DataFrame(
+        data,
+        columns=[
+            "ligand",
+            "DG (kcal/mol)",
+            "uncertainty (kcal/mol)",
+        ],
+    )
+    return df
+
+
+def generate_dg_raw(results_dict: dict[str, dict[str, list]]) -> pd.DataFrame:
+    """
+    Get all the transformation cycle legs found and their DG values.
+
+    Parameters
+    ----------
+    results_dict : dict[str, dict[str, list]]
+        Dictionary of results created by ``extract_results_dict``.
+
+    Returns
+    -------
+    pd.DataFrame
+        A pandas DataFrame with the individual cycle leg dG results.
+    """
+    data = []
+    for lig, results in sorted(results_dict.items()):
+        for simtype, repeats in sorted(results.items()):
+            if simtype != "overall":
+                for repeat in repeats:
+                    m, u = format_estimate_uncertainty(repeat[0].m, repeat[1].m, unc_prec=2)
+                    data.append((simtype, lig, m, u))
+
+    df = pd.DataFrame(
+        data,
+        columns=[
+            "leg",
+            "ligand",
+            "DG (kcal/mol)",
+            "uncertainty (kcal/mol)",
+        ],
+    )
+    return df
+
+
+@click.command("gather-abfe")
+@click.argument(
+    "results",
+    nargs=-1,  # accept any number of results
+    type=click.Path(dir_okay=True, file_okay=True, path_type=pathlib.Path),
+    required=True,
+)
+@click.option(
+    "--report",
+    type=HyphenAwareChoice(["dg", "raw"], case_sensitive=False),
+    default="dg",
+    show_default=True,
+    help=(
+        "What data to report. 'dg' computes the overall binding free energy of each ligand in the dataset (dG), and"
+        "'raw' outputs the raw dG values for each individual leg in the ABFE transformation cycles."
+    ),
+)
+@click.option("output", "-o", type=click.File(mode="w"), default="-")
+@click.option(
+    "--tsv",
+    is_flag=True,
+    default=False,
+    help=(
+        "Results that are output to stdout will be formatted as tab-separated, "
+        "identical to the formatting used when writing to file."
+        "By default, the output table will be formatted for human-readability."
+    ),
+)
+def gather_abfe(
+    results: List[os.PathLike | str],
+    output: os.PathLike | str,
+    report: Literal["dg", "ddg", "raw"],
+    tsv: bool,
+):
+    ddgs = extract_results_dict(results)
+
+    if report == "raw":
+        df = generate_dg_raw(ddgs)
+    elif report == "dg":
+        df = generate_dg(ddgs)
+
+    # write output
+    is_output_file = isinstance(output, click.utils.LazyFile)
+    if is_output_file:
+        click.echo(f"writing {report} output to '{output.name}'")
+    if is_output_file or tsv:
+        df.to_csv(output, sep="\t", lineterminator="\n", index=False)
+
+    # TODO: we can add a --pretty flag if we want this to be optional/preserve backwards compatibility
+    else:
+        rich_print_to_stdout(df)
+
+
+PLUGIN = OFECommandPlugin(
+    command=gather_abfe,
+    section="Quickrun Executor",
+    requires_ofe=(0, 6),
+)
--- openfecli/tests/commands/test_gather.py
+++ openfecli/tests/commands/test_gather.py
@@ -3,6 +3,7 @@ import os
 import pathlib
 from unittest import mock
 
+import pandas as pd
 import pooch
 import pytest
 from click.testing import CliRunner
@@ -14,6 +15,7 @@ from openfecli.commands.gather import (
     format_estimate_uncertainty,
     gather,
 )
+from openfecli.commands.gather_abfe import gather_abfe
 
 from ..conftest import HAS_INTERNET
 from ..utils import assert_click_success
@@ -461,3 +463,34 @@ class TestRBFEGatherFailedEdges:
         result = runner.invoke(gather, results_paths_serial_missing_legs + args + ["--tsv"])
         assert_click_success(result)
         assert "--allow-partial" not in result.output
+
+
+ZENODO_ABFE_DATA = pooch.create(
+    path=POOCH_CACHE,
+    base_url="doi:10.5281/zenodo.17348229",
+    registry={"abfe_results.zip": "md5:547f896e867cce61979d75b7e082f6ba"},
+    retry_if_failed=2,
+)
+
+
+@pytest.fixture
+def abfe_result_dir() -> pathlib.Path:
+    ZENODO_ABFE_DATA.fetch("abfe_results.zip", processor=pooch.Unzip())
+    result_dir = pathlib.Path(POOCH_CACHE) / "abfe_results.zip.unzip/abfe_results/"
+
+    return result_dir
+
+
+class TestGatherABFE:
+    @pytest.mark.parametrize("report", ["raw", "dg"])
+    def test_abfe_full_results(self, abfe_result_dir, report, tmp_path, dataframe_regression):
+        results = [str(abfe_result_dir / f"results_{i}") for i in range(3)]
+        outfile = tmp_path / "out.tsv"
+        args = ["--report", report, "-o", outfile]
+        runner = CliRunner()
+        cli_result = runner.invoke(gather_abfe, results + args + ["--tsv"])
+
+        assert_click_success(cli_result)
+        # TODO: this is an inefficient way to test - when refactoring, we test pull the dfs directly
+        result_df = pd.read_table(outfile)
+        dataframe_regression.check(result_df, default_tolerance=dict(atol=1e-5, rtol=1e-12))
