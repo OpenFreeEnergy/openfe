a1f79bd651b61ad325acf88c59149921958dc872
Alyssa Travitz <alyssa.travitz@omsf.io>
2025-12-16 11:04:30 -0800

    fix path in docs conf

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ import nbformat
 import nbsphinx
 from packaging.version import parse
 
-sys.path.insert(0, os.path.abspath("../"))
+sys.path.insert(0, os.path.abspath("../src/"))
 
 
 os.environ["SPHINX"] = "True"
