acd6d5896755c310b0771a020dcdbda0f78e7d9c
Irfan Alibay <IAlibay@users.noreply.github.com>
2026-01-07 08:15:35 -0500

    Move rfe protocol (#1769)
    Move RFE protocol files around

--- openfe/protocols/openmm_rfe/__init__.py
+++ openfe/protocols/openmm_rfe/__init__.py
@@ -2,11 +2,7 @@
 # For details, see https://github.com/OpenFreeEnergy/openfe
 
 from . import _rfe_utils
-from .equil_rfe_methods import (
-    RelativeHybridTopologyProtocol,
-    RelativeHybridTopologyProtocolResult,
-    RelativeHybridTopologyProtocolUnit,
-)
-from .equil_rfe_settings import (
-    RelativeHybridTopologyProtocolSettings,
-)
+from .equil_rfe_settings import RelativeHybridTopologyProtocolSettings
+from .hybridtop_protocol_results import RelativeHybridTopologyProtocolResult
+from .hybridtop_protocols import RelativeHybridTopologyProtocol
+from .hybridtop_units import RelativeHybridTopologyProtocolUnit
