db646fc5053bf9a1db0e14e1adc22e33336123dc
Alyssa Travitz <alyssa.travitz@omsf.io>
2026-01-22 12:28:04 -0800

    add boilerplate

--- src/openfecli/commands/test.py
+++ src/openfecli/commands/test.py
@@ -1,16 +1,28 @@
 import os
+import pathlib
 import sys
 
 import click
+import pooch
 import pytest
 
 from openfecli import OFECommandPlugin
 from openfecli.utils import write
 
 
+def retrieve_test_data():
+    pass
+
+
 @click.command("test", short_help="Run the OpenFE test suite")
 @click.option('--long', is_flag=True, default=False, help="Run additional tests (takes much longer)")  # fmt: skip
-def test(long):
+@click.option(
+    "--download-only",
+    is_flag=True,
+    default=False,
+    help="Download data to the cache (this is helpful if internet is flaky).",
+)
+def test(long, download_only):
     """
     Run the OpenFE test suite. This first checks that OpenFE is correctly
     imported, and then runs the main test suite, which should take several
@@ -22,6 +34,9 @@ def test(long):
     terminals, these show as green or yellow. Warnings are not a concern.
     However, You should not see anything that fails or errors (red).
     """
+    if download_only:
+        retrieve_test_data()
+        sys.exit()
     try:
         old_env = dict(os.environ)
         os.environ["OFE_SLOW_TESTS"] = str(long)
