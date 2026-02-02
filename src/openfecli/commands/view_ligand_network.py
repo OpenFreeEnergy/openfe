e33127fb4648f0bfd4e0d07823a816248206e03e
Alyssa Travitz <alyssa.travitz@omsf.io>
2026-02-02 14:33:39 -0800

    move to upstream func

--- src/openfecli/commands/view_ligand_network.py
+++ src/openfecli/commands/view_ligand_network.py
@@ -28,11 +28,7 @@ def view_ligand_network(ligand_network: os.PathLike):
 
     network = LigandNetwork.from_graphml(graphml)
     fig = plot_atommapping_network(network)
-    axes = fig.axes
-    for ax in axes:
-        ax.set_frame_on(False)  # remove the black frame
-        for t in ax.texts:
-            t.set_clip_on(False)  # do not clip the label in the network plot
+
     matplotlib.pyplot.show()
 
 
