9ca71b3d0a812c9e53170dfae51aa140c3850bc0
Alyssa Travitz <alyssa.travitz@omsf.io>
2026-02-23 14:04:11 -0800

    raises-> warns fix

--- src/openfe/tests/setup/atom_mapping/test_perses_scorers.py
+++ src/openfe/tests/setup/atom_mapping/test_perses_scorers.py
@@ -18,7 +18,7 @@ USING_OLD_OFF = False
 @pytest.mark.xfail(not USING_OLD_OFF, reason="perses #1108")
 def test_perses_normalization_not_using_positions(gufe_atom_mapping_matrix):
     # now run the openfe equivalent with the same ligand atom _mappings
-    with pytest.raises(DeprecationWarning, match="default_perses_scorer"):
+    with pytest.warns(DeprecationWarning, match="default_perses_scorer"):
         scorer = perses_scorers.default_perses_scorer
     molecule_row = np.max(list(gufe_atom_mapping_matrix.keys())) + 1
     norm_scores = np.zeros([molecule_row, molecule_row])
@@ -42,7 +42,7 @@ def test_perses_normalization_not_using_positions(gufe_atom_mapping_matrix):
 @skip_if_missing("perses")
 @pytest.mark.xfail(not USING_OLD_OFF, reason="perses #1108")
 def test_perses_not_implemented_position_using(gufe_atom_mapping_matrix):
-    with pytest.raises(DeprecationWarning, match="default_perses_scorer"):
+    with pytest.warns(DeprecationWarning, match="default_perses_scorer"):
         scorer = perses_scorers.default_perses_scorer
 
     first_key = list(gufe_atom_mapping_matrix.keys())[0]
@@ -78,7 +78,7 @@ def test_perses_regression(gufe_atom_mapping_matrix):
     assert matrix.shape == (8, 8)
 
     # now run the openfe equivalent with the same ligand atom _mappings
-    with pytest.raises(DeprecationWarning, match="default_perses_scorer"):
+    with pytest.warns(DeprecationWarning, match="default_perses_scorer"):
         scorer = perses_scorers.default_perses_scorer
     scores = np.zeros_like(matrix)
     for (i, j), ligand_atom_mapping in gufe_atom_mapping_matrix.items():
