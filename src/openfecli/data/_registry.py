13786dbe3f3a161bf17969e54a67d1f66d882da4
hannahbaumann <hbaumann@uci.edu>
2026-02-24 13:52:34 +0100

    Update test files

--- src/openfecli/data/_registry.py
+++ src/openfecli/data/_registry.py
@@ -19,9 +19,9 @@ zenodo_rbfe_parallel_data = dict(
     known_hash="md5:ff7313e14eb6f2940c6ffd50f2192181",
 )
 zenodo_abfe_data = dict(
-    base_url="doi:10.5281/zenodo.17348229/",
+    base_url="doi:10.5281/zenodo.18757962/",
     fname="abfe_results.zip",
-    known_hash="md5:547f896e867cce61979d75b7e082f6ba",
+    known_hash="md5:5c4195ae5089b463534896d0dd5dc4a8",
 )
 zenodo_septop_data = dict(
     base_url="doi:10.5281/zenodo.17435569/",
