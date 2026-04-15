6ef7a40c4579e1487a58b3f87e8b63f3bf9f95c0
Hannah Baumann <43765638+hannahbaumann@users.noreply.github.com>
2026-04-15 15:02:07 +0200

    Update src/openfe/protocols/openmm_septop/equil_septop_method.py
    Co-authored-by: Irfan Alibay <IAlibay@users.noreply.github.com>

--- src/openfe/protocols/openmm_septop/equil_septop_method.py
+++ src/openfe/protocols/openmm_septop/equil_septop_method.py
@@ -1147,8 +1147,8 @@ class SepTopProtocol(gufe.Protocol):
         Get the recommended OpenFE settings for this Protocol based on the input states involved in the
         transformation.
 
-        These are intended as a suitable starting point for creating an instance of this protocol, which can be further
-        customized before performing a Protocol.
+        These are intended as a suitable starting point, which can be further
+        customized before creating a Protocol.
 
         Parameters
         ----------
