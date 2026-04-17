3db726956e9ea243a91a77a24e1a8073655f8b36
IAlibay <IAlibay@users.noreply.github.com>
2026-04-17 14:24:16 +0100

    typo

--- src/openfe/tests/protocols/openmm_septop/test_septop_validation.py
+++ src/openfe/tests/protocols/openmm_septop/test_septop_validation.py
@@ -1,6 +1,8 @@
 # This code is part of OpenFE and is licensed under the MIT license.
 # For details, see https://github.com/OpenFreeEnergy/openfe
 import pytest
+
+import openfe
 from openfe import ChemicalSystem, SolventComponent
 from openfe.protocols.openmm_septop import (
     SepTopProtocol,
