5a2674d814f8d8b59d85389a55a7288e78f3ae2a
Alyssa Travitz <alyssa.travitz@omsf.io>
2026-07-29 07:11:29 -0700

    first thoughts on implementation

--- src/openfe/orchestration/exorcist_utils.py
+++ src/openfe/orchestration/exorcist_utils.py
@@ -138,3 +138,11 @@ def get_dependency_df(task_db: exorcist.TaskStatusDB) -> pd.DataFrame:
 
     """
     return pd.read_sql_table("dependencies", task_db.engine)
+
+
+def build_protocol_dag_result_from_task_db():
+    pass
+
+    # identify transformations & their protocol dags
+    # query the status of the units of the dags
+    # collect results and return an iterable of transformations and their protocoldagresults
