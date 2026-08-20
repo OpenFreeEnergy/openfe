8233d33b97e5a581fc1c3f88b5f7b619c41dfe28
Alyssa Travitz <alyssa.travitz@omsf.io>
2026-08-20 12:18:58 -0700

    truncate cli output to seconds

--- src/openfecli/commands/status.py
+++ src/openfecli/commands/status.py
@@ -12,12 +12,43 @@ from openfecli.utils import rich_print_to_stdout
 def status_main(
     task_db_path: pathlib.Path,
 ):
+    """
+    Parameters
+    ----------
+    task_db_path : pathlib.Path
+        Path to a task.db
+
+    Example
+    -------
+    > openfe status task.db
+
+    ┌─────────────────────────────┬──────────────────┬─────────────────────┬───────┬───────────┐
+    │ task_id                     │ status           │ last_modified       │ tries │ max_tries │
+    ├─────────────────────────────┼──────────────────┼─────────────────────┼───────┼───────────┤
+    │ SetupUnit-d568ebe569b445c7… │ COMPLETED        │ 2026-08-14 11:19:16 │ 1     │ 3         │
+    │ SetupUnit-17dc05e0d79747e7… │ COMPLETED        │ 2026-08-14 11:19:21 │ 1     │ 3         │
+    │ SetupUnit-78321eda905c4c2c… │ COMPLETED        │ 2026-08-14 11:19:22 │ 1     │ 3         │
+    │ SetupUnit-0238f65b55044b1e… │ COMPLETED        │ 2026-08-14 11:19:22 │ 1     │ 3         │
+    │ MultiStateSimulationUnit-2… │ COMPLETED        │ 2026-08-14 11:25:10 │ 1     │ 3         │
+    │ MultiStateSimulationUnit-1… │ COMPLETED        │ 2026-08-14 11:25:34 │ 1     │ 3         │
+    │ MultiStateSimulationUnit-f… │ COMPLETED        │ 2026-08-14 11:26:22 │ 1     │ 3         │
+    │ MultiStateSimulationUnit-c… │ TOO_MANY_RETRIES │ 2026-08-14 11:26:24 │ 3     │ 3         │
+    │ MultiStateAnalysisUnit-a72… │ COMPLETED        │ 2026-08-14 11:26:51 │ 1     │ 3         │
+    │ MultiStateAnalysisUnit-e44… │ COMPLETED        │ 2026-08-14 11:26:26 │ 1     │ 3         │
+    │ MultiStateAnalysisUnit-7e9… │ COMPLETED        │ 2026-08-14 11:29:07 │ 1     │ 3         │
+    │ MultiStateAnalysisUnit-72c… │ BLOCKED          │ NaT                 │ 0     │ 3         │
+    └─────────────────────────────┴──────────────────┴─────────────────────┴───────────────────┘
+
+    """
+
     from exorcist import TaskStatusDB
 
     from openfe.orchestration import get_task_df
 
+    # TODO: rewrite this using just sql and rich table
     task_db = TaskStatusDB.from_filename(task_db_path)
     task_df = get_task_df(task_db)
+    task_df["last_modified"] = task_df["last_modified"].dt.floor("s")
     # TODO: add task_type back in once it's used
     rich_print_to_stdout(task_df.drop("task_type", axis=1))
 
@@ -35,10 +66,9 @@ def status_main(
 )
 def status(task_db_path: pathlib.Path):
     """
-    Execute one available task from a warehouse task graph.
+    Show the status of a task.db as a table.
 
-    The warehouse directory must contain a ``tasks.db`` task database and task
-    payloads under ``tasks/`` created via OpenFE orchestration setup.
+
     """
     status_main(task_db_path=task_db_path)
 
