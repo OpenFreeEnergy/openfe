e9d3043795336e5a2ab63d279cc29da9332c07ab
Alyssa Travitz <alyssa.travitz@omsf.io>
2026-08-21 13:07:36 -0700

    bump recent additions to min openfe v1.13

--- src/openfecli/commands/status.py
+++ src/openfecli/commands/status.py
@@ -73,4 +73,4 @@ def status(task_db_path: pathlib.Path):
     status_main(task_db_path=task_db_path)
 
 
-PLUGIN = OFECommandPlugin(command=status, section="Execution", requires_ofe=(1, 12))
+PLUGIN = OFECommandPlugin(command=status, section="Execution", requires_ofe=(1, 13))
