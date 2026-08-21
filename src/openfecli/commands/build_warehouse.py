9e20c67e1c94ce31c612694f1c4eb29f8a50551b
Alyssa Travitz <alyssa.travitz@omsf.io>
2026-08-21 12:31:35 -0700

    add CLI plugin

--- src/openfecli/commands/build_warehouse.py
+++ src/openfecli/commands/build_warehouse.py
@@ -1,6 +1,7 @@
 import click
 from gufe import AlchemicalNetwork
 
+from openfecli import OFECommandPlugin
 from openfecli.parameters import ALCHEMICAL_NETWORK
 from openfecli.utils import print_duration, write
 
@@ -15,8 +16,14 @@ def build_warehouse_main(alchemical_network: AlchemicalNetwork):
     # write('created TaskDB at {}')
 
 
-@ALCHEMICAL_NETWORK.paramter(help=ALCHEMICAL_NETWORK.kwargs["help"])
-@click.command
+@click.command(
+    "build-warehouse",
+    short_help="Build a Warehouse and corresponding TaskDB from an AlchemicalNetwork.",
+)
+@ALCHEMICAL_NETWORK.parameter(help=ALCHEMICAL_NETWORK.kwargs["help"], required=True)
 @print_duration
 def build_warehouse(alchemical_network: AlchemicalNetwork):
     build_warehouse_main(alchemical_network)
+
+
+PLUGIN = OFECommandPlugin(command=build_warehouse, section="Network Planning", requires_ofe=(1, 12))
