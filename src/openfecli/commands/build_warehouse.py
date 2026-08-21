09ece07a7db3a6e606afd706bcf18f33d784cb90
Alyssa Travitz <alyssa.travitz@omsf.io>
2026-08-21 15:55:33 -0700

    clean up default naming

--- src/openfecli/commands/build_warehouse.py
+++ src/openfecli/commands/build_warehouse.py
@@ -8,14 +8,18 @@ from openfecli.parameters import ALCHEMICAL_NETWORK
 from openfecli.utils import print_duration, write
 
 
-def build_warehouse_main(alchemical_network: AlchemicalNetwork):
+def build_warehouse_main(alchemical_network: AlchemicalNetwork, name: str | None = None):
     from openfe.orchestration.exorcist_utils import build_task_db_from_alchemical_network
 
-    task_db, wh = build_task_db_from_alchemical_network(
-        alchemical_network=alchemical_network, warehouse_dir="campaign/", db_path="campaign.db"
+    db_path = Path(f"{name}.db")
+    # TODO: add progress bar
+    _, wh = build_task_db_from_alchemical_network(
+        alchemical_network=alchemical_network,
+        warehouse_dir=Path(name),
+        db_path=db_path,
     )
     write(f"created Warehouse at {wh.root_dir}")
-    # write('created TaskDB at {}')
+    write(f"created TaskDB at {db_path}")
 
 
 @click.command(
@@ -25,8 +29,9 @@ def build_warehouse_main(alchemical_network: AlchemicalNetwork):
 @ALCHEMICAL_NETWORK.parameter(multiple=False, required=True, help=ALCHEMICAL_NETWORK.kwargs["help"])
 @print_duration
 def build_warehouse(alchemical_network: str | Path):
+    name = Path(alchemical_network).stem
     loaded_alch_net = ALCHEMICAL_NETWORK.get(alchemical_network)
-    build_warehouse_main(loaded_alch_net)
+    build_warehouse_main(alchemical_network=loaded_alch_net, name=name)
 
 
 PLUGIN = OFECommandPlugin(command=build_warehouse, section="Network Planning", requires_ofe=(1, 12))
