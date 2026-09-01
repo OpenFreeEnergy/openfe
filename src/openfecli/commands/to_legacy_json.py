83b78edaf31a120c6fbac8d91f70efd60c5b1cc4
Alyssa Travitz <alyssa.travitz@omsf.io>
2026-08-31 17:10:37 -0700

    clean up print statements

--- src/openfecli/commands/to_legacy_json.py
+++ src/openfecli/commands/to_legacy_json.py
@@ -11,16 +11,16 @@ def to_legacy_json_main(warehouse_path: Path, output_dir: Path):
     from openfe.storage.utils import warehouse_to_legacy_json
     from openfe.storage.warehouse import FileSystemWarehouse
 
-    msg = "WARNING! This is an experimental feature and subject to change in a future release of openfe."
-    click.secho(msg, err=True, fg="yellow")  # fmt: skip
-
     write("Loading results from Warehouse ...")
     warehouse = FileSystemWarehouse.from_dir(warehouse_path)
     result_edges = warehouse.gather_all_results()
-    warehouse_to_legacy_json(result_edges, out_dir=output_dir)
-    write(
-        f"Results saved to {output_dir}. You may now run ``openfe gather`` on this directory, if applicable."
-    )
+    if len(result_edges) == 0:
+        write(f"No results found in {warehouse_path}.")
+    else:
+        warehouse_to_legacy_json(result_edges, out_dir=output_dir)
+        write(
+            f"Results saved to {output_dir}. You may now run ``openfe gather`` on this directory, if applicable."
+        )
 
 
 @click.command(
@@ -46,7 +46,8 @@ def to_legacy_json(warehouse_path: str, output_dir: str):
     The warehouse directory must contain a ``tasks.db`` task database and task
     payloads under ``tasks/`` created via OpenFE orchestration setup.
     """
-
+    msg = "WARNING! This is an experimental feature and subject to change in a future release of openfe."
+    click.secho(msg, err=True, fg="yellow")  # fmt: skip
     to_legacy_json_main(warehouse_path=warehouse_path, output_dir=output_dir)
 
 
