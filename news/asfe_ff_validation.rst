d477cfaa61224058025166b1ffdc69b0db3d9d5e
Josh Horton <Josh.Horton@newcastle.ac.uk>
2026-09-09 11:50:37 +0100

    add asfe ff validation

---
+++ news/asfe_ff_validation.rst
@@ -0,0 +1,23 @@
+**Added:**
+
+* <news item>
+
+**Changed:**
+
+* The ``AbsoluteSolvationProtocol`` will now raise a ``ValueError`` during validation if the solvent and vaccum force field settings do not match exactally after excluding the ``nonbonded_method`` field.
+
+**Deprecated:**
+
+* <news item>
+
+**Removed:**
+
+* <news item>
+
+**Fixed:**
+
+* <news item>
+
+**Security:**
+
+* <news item>
--- src/openfe/tests/protocols/openmm_ahfe/test_ahfe_validation.py
+++ src/openfe/tests/protocols/openmm_ahfe/test_ahfe_validation.py
@@ -318,3 +318,19 @@ def test_high_timestep(phase, stateA, stateB):
 
     with pytest.raises(ValueError, match="too large for hydrogen"):
         protocol.validate(stateA=stateA, stateB=stateB, mapping=None)
+
+
+def test_validate_forcefield_settings(stateA, stateB):
+    # make sure the default settings with a different nonbonded method still works
+    settings = AbsoluteSolvationProtocol.default_settings()
+    assert settings.vacuum_forcefield_settings.nonbonded_method == "nocutoff"
+    
+    protocol = AbsoluteSolvationProtocol(settings=settings)
+    protocol.validate(stateA=stateA, stateB=stateB, mapping=None)
+
+    # change some other forcefield settings and make sure an error is raised
+    settings.solvent_forcefield_settings.small_molecule_forcefield = "gaff-2.11"
+    protocol = AbsoluteSolvationProtocol(settings=settings)
+    with pytest.raises(ValueError, match="The following settings differ:\n  small_molecule_forcefield: vacuum=openff-2.2.1, solvent=gaff-2.11"):
+        protocol.validate(stateA=stateA, stateB=stateB, mapping=None)
+    
