bc24b5ef4456caa89be86a09c823672e32ddba2e
Josh Horton <Josh.Horton@newcastle.ac.uk>
2026-09-22 10:32:48 +0100

    turn on corrections by default

--- src/openfe/protocols/openmm_rfe/equil_rfe_settings.py
+++ src/openfe/protocols/openmm_rfe/equil_rfe_settings.py
@@ -100,7 +100,7 @@ class AlchemicalSettings(SettingsBaseModel):
     The minimum distance from the system solutes from which an
     alchemical water can be chosen. Default 0.8 * unit.nanometer.
     """
-    remove_redundant_dummy_atom_connections: bool = False
+    remove_redundant_dummy_atom_connections: bool = True
     """
     If redundant valence terms between dummy and core atoms should be pruned following the best practices of Fleck et al.
     """
